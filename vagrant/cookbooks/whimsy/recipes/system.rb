fd647b8b942a3995ee660edcef0a6bc59ab5067d
Sam Ruby <rubys@apache.org>
2013-06-09 14:33:34 +0000

    export host svn directory as /mnt/svn
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@865027 90ea9780-b833-de11-8433-001ec94261de

--- vagrant/cookbooks/whimsy/recipes/system.rb
+++ vagrant/cookbooks/whimsy/recipes/system.rb
@@ -3,6 +3,27 @@
 # Install update-notifier-common to keep the update-success-stamp current
 #
 
+ruby_block 'upgrade subversion' do
+  block do
+    if File.exist? '/mnt/svn/foundation/.svn/format'
+      cmd = Chef::ShellOut.new(
+        'apt-key adv --keyserver keyserver.ubuntu.com --recv-key A2F4C039 2>&1'
+      ).run_command
+
+      unless cmd.exitstatus == 0
+        Chef::Application.fatal! 'Failed to import subversion signing key'
+      end
+
+      File.open('/etc/apt/sources.list.d/subversion.list', 'w') do |file|
+        file.write <<-EOF.gsub(/^ +/,'')
+          deb http://ppa.launchpad.net/svn/ppa/ubuntu precise main 
+          deb-src http://ppa.launchpad.net/svn/ppa/ubuntu precise main
+        EOF
+      end
+    end
+  end
+end
+
 execute "apt-get-update-periodic" do
   timestamp = '/var/lib/apt/periodic/update-success-stamp'
   command "apt-get update && touch #{timestamp}"
