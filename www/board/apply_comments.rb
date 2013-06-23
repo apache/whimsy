af220ae9acb3ca303b276be325e6c7a6a14b1529
Sam Ruby <rubys@apache.org>
2013-06-23 16:38:51 +0000

    support more patterns
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@867083 90ea9780-b833-de11-8433-001ec94261de

--- www/board/apply_comments.rb
+++ www/board/apply_comments.rb
@@ -26,34 +26,49 @@ def apply_comments(agenda_file, update_file, initials)
   approved = updates['approved']
   comments = updates['comments']
 
-  pattern = /
-      \s{7}See\sAttachment\s\s?(\w+)[^\n]*?\s+
-      \[\s[^\n]*\s*approved:\s*?(.*?)
-      \s*comments:(.*?)\n\s{9}\]
-    /mx
+  patterns = {
+   '' => /
+     ^\s{7}See\sAttachment\s\s?(\w+)[^\n]*?\s+
+     \[\s[^\n]*\s*approved:\s*?(.*?)
+     \s*comments:(.*?)\n\s{9}\]
+     /mx,
 
-  agenda.gsub!(pattern) do |match|
-    attachment, approvals = $1, $2
+   '3' => /
+     ^\s{4}(\w)\.\sThe\smeeting\sof.*?
+     \[\s[^\n]*\s*approved:\s*?(.*?)
+     \s*comments:(.*?)\n\s{9}\]
+     /mx,
 
-    if approved.include? attachment
-      approvals = approvals.strip.split(/(?:,\s*|\s+)/)
-      if approvals.include? initials
-        # do nothing
-      elsif approvals.empty?
-        match[/approved:(\s*)\n/, 1] = " #{initials}"
-      else
-        match[/approved:.*?()\n/, 1] = ", #{initials}"
+   '4' => /
+     ^\s{4}(\w)\.\sPresident\s\[.*?
+     \[\s*comments:()(.*?)\n\s{9}\]
+     /mx,
+  }
+
+  patterns.each do |prefix, pattern|
+    agenda.gsub!(pattern) do |match|
+      attachment, approvals = prefix + $1, $2
+
+      if approved.include? attachment
+	approvals = approvals.strip.split(/(?:,\s*|\s+)/)
+	if approvals.include? initials
+	  # do nothing
+	elsif approvals.empty?
+	  match[/approved:(\s*)\n/, 1] = " #{initials}"
+	else
+	  match[/approved:.*?()\n/, 1] = ", #{initials}"
+	end
       end
-    end
 
-    if comments.include? attachment
-      width = 79-13-initials.length
-      text = comments[attachment].reflow(13+initials.length, width)
-      text[/ *(#{' '*(initials.length+2)})/,1] = "#{initials}: "
-      match[/\n()\s{9}\]/,1] = "#{text}\n"
-    end
+      if comments.include? attachment
+	width = 79-13-initials.length
+	text = comments[attachment].reflow(13+initials.length, width)
+	text[/ *(#{' '*(initials.length+2)})/,1] = "#{initials}: "
+	match[/\n()\s{9}\]/,1] = "#{text}\n"
+      end
 
-    match
+      match
+    end
   end
 
   File.open(agenda_file, 'w') {|file| file.write(agenda)}
