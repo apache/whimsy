aad690f0a10af5a7c2c17eee6065a5bcdf550ad4
Sam Ruby <rubys@apache.org>
2014-01-13 18:18:18 +0000

    stop commits from wiping out 'seen' comments
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@893965 90ea9780-b833-de11-8433-001ec94261de

--- www/test/board/agenda/model/pending.rb
+++ www/test/board/agenda/model/pending.rb
@@ -8,7 +8,7 @@ class Pending
     if File.exist? file
       YAML.load_file(file)
     else
-      {'approved' => [], 'comments' => {}}
+      {'approved' => [], 'comments' => {}, 'seen' => {}}
     end
   end
 
