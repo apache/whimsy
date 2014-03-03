adebd3a1bbc4940272beaf0b903627f2c4e55d04
Sam Ruby <rubys@apache.org>
2014-03-03 19:49:45 +0000

    synchronize initialization of svn
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@899896 90ea9780-b833-de11-8433-001ec94261de

--- asf/svn.rb
+++ asf/svn.rb
@@ -1,17 +1,21 @@
 require 'uri'
+require 'thread'
 
 module ASF
 
   class SVN
     @base = URI.parse('https://svn.apache.org/repos/')
     @mock = 'file:///var/tools/svnrep/'
+    @semaphore = Mutex.new
 
     def self.repos
-      @repos ||= Hash[Dir['/home/whimsysvn/svn/*'].map { |name| 
-        Dir.chdir name.untaint do
-          [`svn info`[/URL: (.*)/,1].sub(/^http:/,'https:'), Dir.pwd.untaint]
-        end
-      }]
+      @semaphore.synchronize do
+        @repos ||= Hash[Dir['/home/whimsysvn/svn/*'].map { |name| 
+          Dir.chdir name.untaint do
+            [`svn info`[/URL: (.*)/,1].sub(/^http:/,'https:'), Dir.pwd.untaint]
+          end
+        }]
+      end
     end
 
     def self.[](name)
