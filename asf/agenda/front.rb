2b9397cf3be8acdacc7ea45e29fe2fc4f146e12a
Sam Ruby <rubys@apache.org>
2014-03-11 16:25:12 +0000

    parse roll call
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@901125 90ea9780-b833-de11-8433-001ec94261de

--- asf/agenda/front.rb
+++ asf/agenda/front.rb
@@ -4,11 +4,34 @@
 
 class ASF::Board::Agenda
   parse do
-    scan @file, /
+    pattern = /
       ^\n\x20(?<section>[12]\.)
       \s(?<title>.*?)\n\n+
       (?<text>.*?)
       (?=\n\s[23]\.)
     /mx
+
+    scan @file, pattern do |attr|
+      if attr['title'] == 'Roll Call'
+        # attempt to identify the people mentioned
+        ASF::Person.preload('cn')
+        list = ASF::Person.list
+        attr['people'] = {}
+        people = attr['text'].scan(/ {8}(\w.*)/).flatten.each do |sname|
+          name = sname
+          sname = sname.strip.downcase.split(/\s+/)
+          list.select do |person|
+            next if person == 'none'
+            pname = person.public_name.downcase.split(/\s+/)
+            if sname.all? {|t1| pname.any? {|t2| t2.start_with? t1}}
+              attr['people'][person.id] = {
+                name: name,
+                member: person.asf_member?
+              }
+            end
+          end
+        end
+      end
+    end
   end
 end
