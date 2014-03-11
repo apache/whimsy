49793921827862b953933f4193c84392466c20f7
Sam Ruby <rubys@apache.org>
2014-03-11 20:52:50 +0000

    Add prior_reports and roster links to change chair and establish PMC resolutions Add people mentioned in the report to change chair resolutions
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@901154 90ea9780-b833-de11-8433-001ec94261de

--- asf/agenda/special.rb
+++ asf/agenda/special.rb
@@ -33,11 +33,36 @@ class ASF::Board::Agenda
       people += text.scan(/#{list_item}<(#{asfid})(?:@|\s*at\s*)
         (?:\.\.\.|apache\.org)>\s*$/x)
 
-      unless people.empty?
-        if text =~ /FURTHER RESOLVED, that ([^,]*?),?\s+be\b/
-          chairname = $1.gsub(/\s+/, ' ').strip
-          chair = people.find {|person| person.first == chairname}
-          attrs['chair'] = (chair ? chair.last : nil)
+      whimsy = 'https://whimsy.apache.org'
+      if people.empty?
+        if title =~ /Change (.*?) Chair/
+          committee = ASF::Committee.find($1)
+          attrs['roster'] =
+            "#{whimsy}/roster/committee/#{CGI.escape committee.name}"
+          attrs['prior_reports'] =
+            "#{whimsy}/board/minutes/#{committee.display_name.gsub(/\W/,'_')}"
+          attrs['people'] = {}
+          committee.members.each do |person|
+            name = person.public_name
+            name.sub!(/ .* /,' ') unless text.include? name
+            if text.include? name
+              attrs['people'][person.id] = {
+                name: name,
+                member: person.asf_member?
+              }
+            end
+          end
+        end
+      else
+        if title =~ /Establish (.*)/
+          name = $1
+          attrs['prior_reports'] =
+            "#{whimsy}/board/minutes/#{name.gsub(/\W/,'_')}"
+          if text =~ /FURTHER RESOLVED, that ([^,]*?),?\s+be\b/
+            chairname = $1.gsub(/\s+/, ' ').strip
+            chair = people.find {|person| person.first == chairname}
+            attrs['chair'] = (chair ? chair.last : nil)
+          end
         end
 
         people.map! do |name, id| 
