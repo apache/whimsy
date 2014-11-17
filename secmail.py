ff8d6b7bc4ee8a869518374c51aae4fcdd9bdc71
Chris Lambertus <cml@apache.org>
2014-11-17 23:54:09 +0000

    Commented out gpg attachment suffix check, as secmail was failing due to an unknown method get_filename.endswith.
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@929587 90ea9780-b833-de11-8433-001ec94261de

--- secmail.py
+++ secmail.py
@@ -178,7 +178,9 @@ def detach(msg):
       if subpayload.get_content_type() == 'image/gif':
         if len(subpayload.get_payload(decode=True))<10240: continue
       # if not subpayload.get_payload(decode=True): continue
-      if subpayload.get_filename().endswith('.gpg'): continue
+
+      # get_filename doesn't appear to have an endswith method
+      # if subpayload.get_filename().endswith('.gpg'): continue
       attachments.append(subpayload)
 
   if len(attachments) == 0: return
