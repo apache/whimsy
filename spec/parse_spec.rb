9b2b09080d8f27f9aa8bbfa60677b584a33dcadd
Sam Ruby <rubys@intertwingly.net>
2015-02-25 16:44:07 -0800

    add parsing unit test

---
+++ spec/parse_spec.rb
@@ -0,0 +1,32 @@
+#
+# Agenda parsing
+#
+
+require_relative 'spec_helper'
+
+feature 'parse' do
+  it "should parse an agenda file" do
+    parsed = Dir.chdir FOUNDATION_BOARD do
+      AGENDA_CACHE.parse('board_agenda_2015_02_18.txt', true)[:parsed]
+    end
+
+    abdera = parsed.find {|item| item['title'] == 'Abdera'}
+    expect(abdera[:attach]).to eq("A")
+    expect(abdera['owner']).to eq('Ant Elder')
+    expect(abdera['missing']).to equal(true)
+    expect(abdera['comments']).to eq('rb: Reminder email sent')
+    expect(abdera['shepherd']).to eq('Rich')
+    expect(abdera[:index]).to eq("Committee Reports")
+
+    aries = parsed.find {|item| item['title'] == 'Aries'}
+    expect(aries[:attach]).to eq("G")
+    expect(aries['owner']).to eq('Jeremy Hughes')
+    expect(aries['missing']).to equal(nil)
+    expect(aries['comments']).to eq('')
+    expect(aries['shepherd']).to eq('Jim')
+    expect(aries['approved']).to include('sr')
+
+    actions = parsed.find {|item| item['title'] == 'Action Items'}
+    expect(actions['actions']['Rave']).to include(/Sam:/)
+  end
+end
