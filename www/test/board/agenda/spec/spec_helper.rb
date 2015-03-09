f0d2603b918ffa88f9a3ae16b380678d284822e3
Sam Ruby <rubys@apache.org>
2015-03-09 13:35:20 +0000

    svn cleanup
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@943014 90ea9780-b833-de11-8433-001ec94261de

--- www/test/board/agenda/spec/spec_helper.rb
+++ www/test/board/agenda/spec/spec_helper.rb
@@ -5,7 +5,15 @@
 ENV['RACK_ENV'] = 'test'
 ENV['REMOTE_USER'] = 'test'
 require 'capybara/rspec'
-require 'capybara/poltergeist'
 require_relative '../main'
 Capybara.app = Sinatra::Application
 Capybara.javascript_driver = :poltergeist
+
+# only load poltergeist driver for JavaScript if phantomjs is available
+if
+  ENV['PATH'].split(File::PATH_SEPARATOR).any? do |path|
+    File.exist? File.join(path, 'phantomjs')
+  end
+then
+  require 'capybara/poltergeist'
+end
