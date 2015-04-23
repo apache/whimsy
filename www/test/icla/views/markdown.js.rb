87d820f12a54dad9bb28c8dda3f264854017d983
Sam Ruby <rubys@apache.org>
2015-04-23 14:11:44 +0000

    deploy
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@948889 90ea9780-b833-de11-8433-001ec94261de

---
+++ www/test/icla/views/markdown.js.rb
@@ -0,0 +1,37 @@
+#
+# Convert markdown to html
+#
+
+class Markdown < React
+  def componentWillMount()
+    self.componentWillReceiveProps()
+  end
+
+  def componentWillReceiveProps()
+
+    # trim leading and trailing spaces
+    text = @@text.sub(/^\s*\n/, '').sub(/\s+$/, '')
+
+    # normalize indentation
+    spaces = new RegExp('^ *\S', 'mg');
+    match = regexp.exec(text)
+    if match
+      indent = match[0].length - 1
+      while (match = regexp.exec(text))
+        indent = match[0].length - 1 if indent >= match[0].length
+      end
+
+      if indent > 0
+        spaces = Array.new(indent+1).join(' ')
+        text = text.replace("^#{spaces}", 'g'), '')
+      end
+    end
+
+    # convert markdown to text
+    @html = markd(text)
+  end
+
+  def render
+    _span dangerouslySetInnerHTML: { __html: @html }
+  end
+end
