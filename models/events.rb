a70cf8313dd89ec7dee9cb375e1b06a0cb614144
Sam Ruby <rubys@intertwingly.net>
2015-05-03 18:41:33 -0400

    support restart even on Ruby 2.0 and Ruby 2.1

--- models/events.rb
+++ models/events.rb
@@ -7,8 +7,7 @@
 class Events < Queue
   @@subscriptions = []
 
-  @@restart_usr2 = nil
-  @@restart_hup = nil
+  @@restart = false
 
   # create a new subscription
   def self.subscribe
@@ -32,24 +31,42 @@ class Events < Queue
   # When restart signal is detected, close all open connections
   def self.hook_restart
     # puma uses SIGUSR2
-    @@restart_usr2 ||= trap 'SIGUSR2' do
-      @@restart_usr2.call if Proc === @@restart_usr2
-      Events.post(:exit)
+    restart_usr2 ||= trap 'SIGUSR2' do
+      restart_usr2.call if Proc === restart_usr2
+      begin
+        Events.post(:exit)
+      rescue ThreadError
+        # some versions of Ruby don't allow queue operations in traps
+        @restart = true
+      end
     end
 
     # thin uses SIGHUP
-    @@restart_hup ||= trap 'SIGHUP' do
-      @@restart_hup.call if Proc === @@restart_hup
-      Events.post(:exit)
+    restart_hup ||= trap 'SIGHUP' do
+      restart_hup.call if Proc === restart_hup
+      begin
+        Events.post(:exit)
+      rescue ThreadError
+        # some versions of Ruby don't allow queue operations in traps
+        @restart = true
+      end
     end
   end
 
   # As some TCP/IP implementations will close idle sockets after as little
-  # as 30 seconds, sent out a heartbeat every 25 seconds
+  # as 30 seconds, sent out a heartbeat every 25 seconds.  Due to limitations
+  # of some versions of Ruby (2.0, 2.1), this is lowered to every 5 seconds
+  # in development mode.
   Thread.new do
     loop do
-      sleep 25
-      Events.post(:heartbeat)
+      sleep(ENV['RACK_ENV'] == 'development' ? 5 : 25)
+
+      if @restart
+        Events.post(:exit)
+        @restart = false
+      else
+        Events.post(:heartbeat)
+      end
     end
   end
 end
