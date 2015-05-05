a8f1af83deb3127e1fdbd5996eb59119b1991ef8
Sam Ruby <rubys@intertwingly.net>
2015-05-05 12:17:44 -0400

    backwards compatibility for Opera and old versions of IE

--- views/models/events.js.rb
+++ views/models/events.js.rb
@@ -43,6 +43,10 @@ class Events
     # determine localStorage variable prefix based on url up to the date
     base = document.getElementsByTagName("base")[0].href
     origin = location.origin
+    if not origin # compatibility: http://s.apache.org/X2L
+      origin = window.location.protocol + "//" + window.location.hostname + 
+        (window.location.port ? ':' + window.location.port : '')
+    end
     @@prefix = base[origin.length..-1].sub(/\/\d{4}-\d\d-\d\d\/.*/, '').
       gsub(/^\W+|\W+$/, '').gsub(/\W+/, '_') || location.port
 
