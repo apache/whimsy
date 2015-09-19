e3aad7fa81a9ba6435d87396c84ec6e0d45dc30b
Sam Ruby <rubys@apache.org>
2015-09-19 17:18:35 +0000

    Add a command line interface to sort ICLAs
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@966038 90ea9780-b833-de11-8433-001ec94261de

---
+++ www/secretary/iclasort.rb
@@ -0,0 +1,22 @@
+require 'whimsy/asf'
+
+OFFICERS = ASF::SVN['private/foundation/officers']
+
+unless OFFICERS
+  STDERR.puts 'Unable to locate a checked out version of '
+  STDERR.puts 'https://svn.apache.org/repos/private/foundation/officers.'
+  STDERR.ptus
+  STDERR.puts "Please check your #{Dir.home}/.whimsy file"
+  exit 1
+end
+
+Dir.chdir OFFICERS
+source = File.read('iclas.txt')
+sorted = ASF::ICLA.sort(source)
+
+if source == sorted
+  puts 'no change'
+else
+  File.write('iclas.txt', sorted)
+  system 'svn diff iclas.txt'
+end
