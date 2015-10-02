88326a3cc596d36db29ce5e183ca0bf041d9f7b2
Sam Ruby <rubys@apache.org>
2015-10-02 14:16:39 +0000

    oops, forgot a file
    git-svn-id: https://svn.apache.org/repos/infra/infrastructure/trunk/projects/whimsy@967516 90ea9780-b833-de11-8433-001ec94261de

---
+++ www/test/roster/banner.rb
@@ -0,0 +1,26 @@
+#
+# common banner
+#
+
+class Wunderbar::HtmlMarkup
+  def _banner(args)
+    # logo
+    _a href: 'http://whimsy.apache.org/' do
+      _img src: 'https://id.apache.org/img/asf_logo_wide.png',
+        alt: 'ASF Logo', title: 'ASF Logo'
+    end
+
+    # breadcrumbs
+    _div.breadcrumbs do
+      _a href: 'http://www.apache.org' do
+        _span.glyphicon.glyphicon_home
+      end
+
+      _a 'whimsy', href: 'https://www.whimsy.org/'
+      args[:breadcrumbs].each do |name, link|
+        _span "\u00BB"
+        _a name.to_s, href: link
+      end
+    end
+  end
+end
