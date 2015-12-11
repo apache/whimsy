38ac6d797e67c56b5c89345d10b0be345779fa24
Sam Ruby <rubys@intertwingly.net>
2015-12-11 03:32:47 -0500

    another jira delimiter (as seen on REEF 2015-12-16)

--- views/pages/report.js.rb
+++ views/pages/report.js.rb
@@ -219,8 +219,8 @@ class Report < React
   # hotlink to JIRA issues
   def jira(text)
     jira_issue =
-      Regexp.new(/(^|\s|\()([A-Z][A-Z0-9]+)-([1-9][0-9]*)
-        (\.(\D|$)|[,;:\s)]|$)/x, 'g')
+      Regexp.new(/(^|\s|\(|\[)([A-Z][A-Z0-9]+)-([1-9][0-9]*)
+        (\.(\D|$)|[,;:\s)\]]|$)/x, 'g')
 
     text.gsub! jira_issue do |m, pre, name, issue, post|
       if JIRA.find(name)
