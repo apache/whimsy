db1f067edc85ab91fd37f34aa011f6326b1c4fe0
Sam Ruby <rubys@intertwingly.net>
2015-12-14 17:40:50 -0500

    rough in UI for drag/drop

--- www/secmail/views/parts.js.rb
+++ www/secmail/views/parts.js.rb
@@ -4,9 +4,22 @@ class Parts < React
   end
 
   def render
+    # common options for all list items
+    options = {
+      draggable: 'true',
+      onDragStart: self.dragStart,
+      onDragEnter: self.dragEnter,
+      onDragOver: self.dragOver,
+      onDragLeave: self.dragLeave,
+      onDragEnd: self.dragEnd,
+      onDrop: self.drop,
+      onContextMenu: self.menu,
+    }
+
     _ul @@attachments do |attachment|
-      _li onContextMenu: self.menu do
-        _a attachment.name, href: attachment.name, target: 'content'
+      _li options do
+        _a attachment.name, href: attachment.name, target: 'content',
+          draggable: 'false'
       end
     end
 
@@ -31,7 +44,6 @@ class Parts < React
     $menu.style.display = :block
     event.preventDefault()
   end
-
   # hide context menu whenever a click is received outside the menu
   def click(event)
     target = event.target
@@ -40,5 +52,57 @@ class Parts < React
       target = target.parentNode
     end
     $menu.style.display = :none
+  end
+
+  #
+  # drag/drop support.  Note: support varies by browser (in particular,
+  # when events are called and whether or not a particular event has
+  # access to dataTransfer data.)  Accordingly, the below is coded in
+  # a way that is mildly redundant and uses React.js state data in lieu of
+  # dataTransfer.  Oddly, with some browsers, drag and drop isn't possible
+  # without setting something in dataTransfer, so that data is set too, even
+  # though it is not used.
+  #
+
+  # start by capturing the 'href' attribute
+  def dragStart(event)
+    @drag = event.currentTarget.querySelector('a').getAttribute('href')
+    event.dataTransfer.setData('text', @drag)
+  end
+
+  # show item as valid drop target when a dragged element is over it
+  def dragEnter(event)
+    href = event.currentTarget.querySelector('a').getAttribute('href')
+    if @drag and @drag != href
+      event.currentTarget.classList.add 'drop-target'
+    end
+  end
+
+  # check for valid drag/drop operations (different href)
+  def dragOver(event)
+    href = event.currentTarget.querySelector('a').getAttribute('href')
+    if @drag and @drag != href
+      event.currentTarget.classList.add 'drop-target'
+      event.preventDefault()
+    end
+  end
+
+  # unmark item as selected when a dragged element is no longer over it
+  def dragLeave(event)
+    event.currentTarget.classList.remove 'drop-target'
+  end
+
+  # complete drop operation
+  def drop(event)
+    href = event.currentTarget.querySelector('a').getAttribute('href')
+    alert("drop #{@drag} onto #{href}")
+    @drag = nil
+    event.currentTarget.classList.remove 'drop-target'
+    event.preventDefault()
+  end
+
+  # cancel drag operation
+  def dragEnd(event)
+    @drag = nil
   end
 end
