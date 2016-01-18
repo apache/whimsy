65ba363edf936afc41a7e815195e688ca5e4a958
Sam Ruby <rubys@intertwingly.net>
2016-01-17 20:02:55 -0500

    use info status when board minutes are updated

--- www/status/monitors/board_minutes.rb
+++ www/status/monitors/board_minutes.rb
@@ -4,12 +4,18 @@
 
 def Monitor.board_minutes(previous_status)
   index = File.expand_path('../../www/board/minutes/index.html')
-  log = File.expand_path('../../www/logs/collate_minutes')
+  log = File.read(File.expand_path('../../www/logs/collate_minutes'))
 
-  if File.read(log) =~ /\*\*\* (Exception.*) \*\*\*/
+  if log =~ /\*\*\* (Exception.*) \*\*\*/
     {
       level: 'danger',
       data: $1,
+      href: '../logs/collate_minutes'
+    }
+  elsif log.length > 0
+    {
+      level: 'info',
+      data: "Last updated: #{File.mtime(index)}",
       href: '../logs/collate_minutes'
     }
   else
