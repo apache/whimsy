3d7d8af88265319bfa4ef16820364a0b32640fdb
Sebb <sebb@apache.org>
2016-02-16 01:23:43 +0000

    Simplify by using json_common

--- www/roster/public_icla_info.rb
+++ www/roster/public_icla_info.rb
@@ -1,28 +1,6 @@
-require 'bundler/setup'
+# Extract public data from iclas.txt
 
-require 'whimsy/asf'
-
-GEMVERSION = Gem.loaded_specs['whimsy-asf'].version.to_s rescue nil
-  # rescue is to allow for running with local library rather than a Gem
-
-# parse arguments for output file name
-if ARGV.length == 0 or ARGV.first == '-'
-  output = STDOUT
-else
-  # exit quickly if there has been no change
-  if File.exist? ARGV.first
-    source = "#{ASF::SVN['private/foundation/officers']}/iclas.txt"
-    lib = File.expand_path('../../../lib', __FILE__)
-    mtime = Dir["#{lib}/**/*"].map {|file| File.mtime(file)}.max
-    mtime = [mtime, File.mtime(source), File.mtime(__FILE__)].max
-    if File.mtime(ARGV.first) >= mtime
-      previous_results = JSON.parse(File.read ARGV.first) rescue {}
-      exit 0 if previous_results['gem_version'] == GEMVERSION
-    end
-  end
-
-  output = File.open(ARGV.first, 'w')
-end
+require_relative 'public_json_common'
 
 # gather icla info
 ids = {}
@@ -38,11 +16,8 @@ end
 
 info = {
   last_updated: ASF::ICLA.svn_change,
-  gem_version: GEMVERSION,
   committers: ids.sort,
   non_committers: noid # do not sort because the input is already sorted by surname
 }
 
-# output results
-output.puts JSON.pretty_generate(info)
-output.close
+public_json_output(info)
