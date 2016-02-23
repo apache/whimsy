76d580d72468070280032ef462684859530b6ae6
Sam Ruby <rubys@intertwingly.net>
2016-02-23 10:58:29 -0500

    make nominees list eligable for GC

--- lib/whimsy/asf/nominees.rb
+++ lib/whimsy/asf/nominees.rb
@@ -1,9 +1,14 @@
+require 'weakref'
+
 module ASF
 
   class Person < Base
   
     def self.member_nominees
-      return @member_nominees if @member_nominees
+      begin
+        return Hash[@member_nominees.to_a] if @member_nominees
+      rescue NoMethodError, WeakRef::RefError
+      end
 
       meetings = ASF::SVN['private/foundation/Meetings']
       nominations = Dir["#{meetings}/*/nominated-members.txt"].sort.last.untaint
@@ -22,7 +27,8 @@ module ASF
         nominees[find(id)] = nomination
       end
 
-      @member_nominees = nominees
+      @member_nominees = WeakRef.new(nominees)
+      nominees
     end
 
     def member_nomination
