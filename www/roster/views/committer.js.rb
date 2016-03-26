04de0c90d42810be36ae5e8c189a62dbb185c218
Sam Ruby <rubys@intertwingly.net>
2016-03-26 16:18:36 -0400

    rough in UI

--- www/roster/views/committer.js.rb
+++ www/roster/views/committer.js.rb
@@ -149,10 +149,21 @@ class Committer < React
 
       if @committer.member
         if @committer.member.status
-          _tr data_edit: ('memstat' if true) do
+          _tr data_edit: ('memstat' if @@auth.secretary) do
             _td 'Member status'
             if @committer.member.info
-              _td @committer.member.status
+              _td do
+                _span @committer.member.status
+               if @edit_memstat
+                 if @committer.member.status.include? 'Active'
+                   _button.btn.btn_primary 'move to emeritus',
+                     disabled: true
+                 elsif @committer.member.status.include? 'Emeritus'
+                   _button.btn.btn_primary 'move to active',
+                     disabled: true
+                 end
+               end
+              end
             else
               _td.not_found 'Not in members.txt'
             end
