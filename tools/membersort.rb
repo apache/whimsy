ac8bc5506f658150a750eccdac983f6340c0038d
Craig L Russell <craig.russell@oracle.com>
2016-03-27 18:03:14 -0700

    Add utility to sort members.txt

---
+++ tools/membersort.rb
@@ -0,0 +1,23 @@
+# svn update and sort the members.txt file and show the differences
+
+$LOAD_PATH.unshift File.realpath(File.expand_path('../../lib', __FILE__))
+require 'whimsy/asf'
+
+FOUNDATION = ASF::SVN['private/foundation']
+
+Dir.chdir FOUNDATION
+
+members = FOUNDATION + '/members.txt'
+puts 'svn update ' + members
+system 'svn update ' + members
+
+source = File.read('members.txt')
+sorted = ASF::Member.sort(source)
+
+if source == sorted
+  puts 'no change'
+else
+  File.write('members.txt', sorted)
+  system 'svn diff members.txt'
+end
+
