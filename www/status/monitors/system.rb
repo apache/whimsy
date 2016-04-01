62df2de2682ab99e11ca7797f2f714beab1c7368
Sebb <sebb@apache.org>
2016-04-01 19:44:33 +0100

    Initial code for system-related status checks

---
+++ www/status/monitors/system.rb
@@ -0,0 +1,39 @@
+#
+# Monitor status of system
+# Currently only checks puppet
+#
+
+require 'time'
+
+def Monitor.system(previous_status)
+  name='puppet'
+  status = {}
+  status[name] = {
+    mtime: Time.now.gmtime.iso8601,
+    command: 'service puppet status'
+  }
+  begin
+    puppet = `service puppet status`.strip
+    if puppet.include? ' * agent is running'
+      status[name].merge! level: 'warning', title: puppet
+    end
+    rescue Exception => e
+      status[name] = {
+        level: 'danger',
+        data: {
+          exception: {
+            level: 'danger',
+            text: e.inspect,
+            data: e.backtrace
+          }
+        }
+      }
+  end
+  {data: status}
+end
+
+# for debugging purposes
+if __FILE__ == $0
+  require 'json'
+  puts JSON.pretty_generate(Monitor.system(nil))
+end
