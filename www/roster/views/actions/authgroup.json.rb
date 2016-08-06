213fed49cf908def23ee4cc2d66eadefc28d213f
Sam Ruby <rubys@intertwingly.net>
2016-08-06 16:52:26 -0400

    whoops, forgot to commit a file

---
+++ www/roster/views/actions/authgroup.json.rb
@@ -0,0 +1,43 @@
+#
+# Add or remove a person from an authorization group in LDAP
+#
+
+if env.password
+  person = ASF::Person.find(@id)
+  group = ASF::AuthGroup.find(@group)
+
+  # update LDAP
+  ASF::LDAP.bind(env.user, env.password) do
+    if @action == 'add'
+      group.add(person)
+    elsif @action == 'remove'
+      group.remove(person)
+    end
+  end
+
+  # compose E-mail
+  action = (@action == 'add' ? 'added to' : 'removed from')
+  list = "#{@group} authorization group"
+
+  details = [person.dn, group.dn]
+
+  from = ASF::Person.find(env.user)
+
+  mail = Mail.new do
+    from "#{from.public_name} <#{from.id}@apache.org>".untaint
+    to "root@apache.org"
+    subject "#{person.public_name} #{action} #{list}"
+    body "Current roster can be found at:\n\n" +
+      "  https://whimsy.apache.org/roster/group/#{group.id}\n\n" +
+      "LDAP details:\n\n  #{details.join("\n  ")}"
+  end
+
+  # Header for root@'s lovely email filters
+  mail.header['X-For-Root'] = 'yes'
+
+  # deliver email
+  mail.deliver!
+end
+
+# return updated committee info to the client
+Group.serialize(@group)
