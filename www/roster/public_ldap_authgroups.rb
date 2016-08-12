9b01bf58c6c1e9122c2ba1eca44086c58e92ab99
Sebb <sebb@apache.org>
2016-08-12 21:47:01 +0100

    += code to generate public_ldap_authgroups.json

---
+++ www/roster/public_ldap_authgroups.rb
@@ -0,0 +1,54 @@
+# Creates JSON output with the following format:
+#
+# {
+# "lastTimestamp": "20160807004722Z",
+# "auth": {
+#   "accounting": {
+#     "createTimestamp": "20160802141719Z",
+#     "modifyTimestamp": "20160806162424Z",
+#     "roster": [
+#     ...
+#     ]
+#   },
+#   "apachecon": {
+#     "createTimestamp": "20160802141719Z",
+#     "modifyTimestamp": "20160802141719Z",
+#     "roster": [
+#     ...
+#   },
+# }
+#
+
+require_relative 'public_json_common'
+
+# gather auth group info
+entries = {}
+
+groups = ASF::AuthGroup.preload # for performance
+
+if groups.empty?
+  Wunderbar.error "No results retrieved, output not created"
+  exit 0
+end
+
+lastStamp = ''
+groups.keys.sort_by {|a| a.name}.each do |entry|
+    m = []
+    entry.members.sort_by {|a| a.name}.each do |e|
+        m << e.name
+    end
+    lastStamp = entry.modifyTimestamp if entry.modifyTimestamp > lastStamp
+    entries[entry.name] = {
+        createTimestamp: entry.createTimestamp,
+        modifyTimestamp: entry.modifyTimestamp,
+        roster: m 
+    }
+end
+
+info = {
+  # Is there a use case for the last createTimestamp ?
+  lastTimestamp: lastStamp,
+  auth: entries,
+}
+
+public_json_output(info)
