f26577b45a1df5c24e2a4e7b82da74ba793fad1e
Shane Curcuru <asf@shanecurcuru.org>
2017-04-12 21:16:59 -0400

    Improve header

--- lib/whimsy/asf/themes.rb
+++ lib/whimsy/asf/themes.rb
@@ -11,28 +11,36 @@ class Wunderbar::HtmlMarkup
       end
     else
       _div.header.container_fluid do
-        _ul class: 'nav nav-tabs' do
-          _li role: 'presentation' do
+        _div.row do
+          _div.col_sm_4.hidden_xs do
             _a href: 'https://www.apache.org/' do
               _img title: 'ASF Logo', alt: 'ASF Logo', width: 250, height: 101,
+                style: "margin-left: 10px; margin-top: 10px;",
                 src: 'https://www.apache.org/foundation/press/kit/asf_logo_small.png'
             end
           end
-          _li role: 'presentation' do
+          _div.col_sm_2.col_xs_2 do
             _a href: '/' do
               _img title: 'Whimsy logo', alt: 'Whimsy hat', src: 'https://whimsy.apache.org/whimsy.svg', height: 101 
             end
           end
-          _li role: 'presentation' do
-            _a 'Mailing list', href: 'https://lists.apache.org/list.html?dev@whimsical.apache.org'
+          _div.col_sm_6.col_xs_9.align_bottom do 
+            _ul class: 'nav nav-tabs' do
+              _li role: 'presentation' do
+                _a 'Code', href: 'https://github.com/apache/whimsy/'
+              end
+              _li role: 'presentation' do
+                _a 'Questions', href: 'https://lists.apache.org/list.html?dev@whimsical.apache.org'
+              end
+              _li role: 'presentation' do
+                _a 'About this site', href: '/technology'
+              end
+              _li role: 'presentation' do
+                _span.badge id: 'script-ok'
+              end
+            end
           end
-          _li role: 'presentation' do
-            _a 'About this site', href: '/technology'
-          end
-          _li role: 'presentation' do
-            _span.badge id: 'script-ok'
-          end
-        end
+        end      
         _h1 title
       end
     end
