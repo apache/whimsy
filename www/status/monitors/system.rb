819eb0783552c36740bd1efaad48d6c137f484a8
Sam Ruby <rubys@intertwingly.net>
2017-04-15 15:19:52 -0400

    use utf-8 for puppet status

--- www/status/monitors/system.rb
+++ www/status/monitors/system.rb
@@ -11,25 +11,33 @@ def Monitor.system(previous_status)
   status[name] = {
     command: 'service puppet status',
   }
+
   begin
-    puppet = `service puppet status`.strip
+    env['LC_ALL'] = 'en_US.UTF-8'
+    env['LANG'] = 'en_US.UTF-8'
+    env['LANGUAGE'] = 'en_US.UTF-8'
+
+    puppet = `service puppet status`.force_encoding('utf-8').strip
+
     if puppet.include? '* agent is running'
       status[name].merge! level: 'success', data: puppet
     else
       status[name].merge! level: 'warning', data: puppet
     end
-    rescue Exception => e
-      status[name] = {
-        level: 'danger',
-        data: {
-          exception: {
-            level: 'danger',
-            text: e.inspect,
-            data: e.backtrace
-          }
-        }
+
+  rescue Exception => e
+    status[name] = {
+      level: 'danger',
+      data: {
+	exception: {
+	  level: 'danger',
+	  text: e.inspect,
+	  data: e.backtrace
+	}
       }
+    }
   end
+
   {data: status}
 end
 
