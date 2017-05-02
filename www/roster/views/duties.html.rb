3b4e3e1c357a1dd08b01e186fa471791e5356f56
Shane Curcuru <asf@shanecurcuru.org>
2017-05-02 14:16:52 -0400

    Annotate which data bits are private

--- www/roster/views/duties.html.rb
+++ www/roster/views/duties.html.rb
@@ -1,7 +1,7 @@
 #
 # Organization Chart
-#
-
+# NOTE: this view of the data is Member-private
+PVTCLASS = 'text-warning'
 _html do
   _title @role['info']['role']
   _base href: '..'
@@ -19,19 +19,25 @@ _html do
    id = @role['info']['id'] || @role['info']['chair']
   _h1_ "#{@role['info']['role']} - #{ASF::Person.find(id).public_name}"
 
-  _h2_ 'Quick Info'
+  _h2_ do
+    _ 'Quick Info'
+    _small do 
+      _ ' - includes '
+      _span 'Member private data', class: PVTCLASS
+    end
+  end
 
   _table.table do
     _tbody do
       @role['info'].each do |key, value|
         next if key == 'role'
         next unless value
-
+        (key =~ /private/i) ? (pvt = PVTCLASS) : (pvt = '')
         _tr_ do
-          _td key
+          _td key, class: pvt
 
           if %w(id chair).include? key
-            _td do
+            _td class: pvt do
               if value == 'tbd'
                 _span value
               else
@@ -39,7 +45,7 @@ _html do
               end
             end
           elsif %w(reports-to).include? key
-            _td! do
+            _td! class: pvt do
               value.split(/[, ]+/).each_with_index do |role, index|
                 _span ', ' if index > 0
                 if role == 'members'
@@ -50,11 +56,11 @@ _html do
               end
             end
           elsif %w(email).include? key
-            _td do
+            _td class: pvt do
               _a value, href: "mailto:#{value}"
             end
           elsif %w(private-list).include? key
-            _td do
+            _td class: pvt do
               if value == 'board-private@apache.org'
                 _a value, href: "mailto:#{value}"
               else
@@ -62,11 +68,11 @@ _html do
               end
             end
           elsif %w(roster resolution).include? key
-            _td do
+            _td class: pvt do
               _a value, href: value
             end
           else
-            _td value
+            _td value, class: pvt 
           end
         end
       end
@@ -99,7 +105,7 @@ _html do
 
   @role.each do |title, text|
     next if title == 'info' or title == 'mtime'
-    _h2.text_capitalize title
+    (title =~ /private/i) ? _h2.text_capitalize.text_warning(title) : _h2.text_capitalize(title)
     _markdown text
   end
 end
