13a67db920bb5539841b7bf27978b8bc205b1864
Sebb <sebb@apache.org>
2017-06-19 13:05:12 +0100

    Add method projectlist to return the groups using ou=project

--- lib/whimsy/asf/auth.rb
+++ lib/whimsy/asf/auth.rb
@@ -16,21 +16,21 @@ module ASF
       @file = file
     end
 
-    # Iteratively return each entry in the authorization file as a pair
+    # Iteratively return each non_LDAP entry in the authorization file as a pair
     # of values: a name and list of ids.
     def each
-      # TODO - should this read the Git repo directly?
-      auth = ASF::Git.find('infrastructure-puppet')
-      if auth
-        auth += '/modules/subversion_server/files/authorization'
-      else
-        # SVN copy is no longer in use - see INFRA-11452
-        raise Exception.new("Cannot find Git: infrastructure-puppet")
-      end
-
-      File.read("#{auth}/#{@file}-authorization-template").
+      read_auth.
         scan(/^([-\w]+)=(\w.*)$/).each do |pmc, ids|
         yield pmc, ids.split(',')
+      end
+    end
+
+    # Iteratively return each ou=project entry in the authorization file as the group name
+    def projects
+      #incubator={ldap:cn=incubator,ou=project,ou=groups,dc=apache,dc=org;attr=member}
+      read_auth.
+        scan(/^\w[^=]+={ldap:cn=(\w[^,]+),ou=project,ou=groups/).each do |group|
+        yield group[0]
       end
     end
 
@@ -39,6 +39,22 @@ module ASF
       def to_h
         Hash[self.to_a]
       end
+    end
+
+    private
+
+    def read_auth
+      # TODO - should this read the Git repo directly?
+      # Probably not: this file is read frequently so would need to be cached anyway
+      # The Git clone is updated every 10 minutes which should be sufficiently recent
+      auth = ASF::Git.find('infrastructure-puppet')
+      if auth
+        auth += '/modules/subversion_server/files/authorization'
+      else
+        # SVN copy is no longer in use - see INFRA-11452
+        raise Exception.new("Cannot find Git: infrastructure-puppet")
+      end
+      File.read("#{auth}/#{@file}-authorization-template")
     end
   end
 
