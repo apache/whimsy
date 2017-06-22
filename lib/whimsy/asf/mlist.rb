5138fd95bd373a0e429f01b92c0dd396de544a58
Sebb <sebb@apache.org>
2017-06-22 01:30:27 +0100

    Initial version of mail list utility module

---
+++ lib/whimsy/asf/mlist.rb
@@ -0,0 +1,96 @@
+module ASF
+
+  module MLIST
+    # utility methods for handling mailing list subscriptions and moderations
+
+    # whilst the source files are not particularly difficult to parse, it makes
+    # sense to centralise access so any necessary changes can be localised
+
+    # Potentially also the methods could check if access was allowed.
+    # This is currently done by the callers
+    
+    def self.board_subscribers
+      # yield the list of board subscribers
+      File.readlines(BOARD_SUBSCRIPTIONS).each do |line|
+        yield line
+      end
+    end
+
+    def self.members_subscribers
+      # yield the list of subscribers to members@
+      File.readlines(MEMBERS_SUBSCRIPTIONS).each do |line|
+        yield line
+      end
+    end
+
+    def self.incubator_mods
+      # return a hash of incubator moderators
+      # list-name => [subscribers]
+      moderators = Hash[File.read(LIST_MODS).split(/\n\n/).
+        select {|k,v| k =~ /incubator.apache.org/ && k !~ /\/infra-(dev2?|[a-z])\//}.
+        map {|stanza| [stanza[/incubator.apache.org\/(.*)\//,1],
+        stanza.scan(/^(.*@.*)/).flatten]}]
+      moderators
+    end
+
+    def self.subscriptions(emails, response = {})
+      # return a hash of subscriptions for the list of emails provided
+      
+      return response unless File.exists? LIST_SUBS
+
+      response[:subscriptions] = []
+      response[:subtime] = File.mtime(LIST_SUBS)
+
+      # File format
+      # blank line
+      # /home/apmail/lists/accumulo.apache.org/commits
+      # archive@mail-archive.com
+      # ...
+      File.read(LIST_SUBS).split(/\n\n/).each do |stanza|
+        # list names can include '-': empire-db
+        list = stanza.match(/\/([-\w]*\.?apache\.org)\/(.*?)(\n|\Z)/)
+
+        subs = stanza.scan(/^(.*@.*)/).flatten
+        emails.each do |email|
+          if subs.include? email
+            response[:subscriptions] << ["#{list[2]}@#{list[1]}", email]
+          end
+        end
+      end
+      response
+    end
+
+    def self.moderates(user_emails, response = {})
+      # return the mailing lists which are moderated by any of the list of emails
+
+      return response unless File.exists? LIST_MODS
+
+      response[:moderates] = {}
+      response[:modtime] = File.mtime(LIST_MODS)
+      moderators = File.read(LIST_MODS).split(/\n\n/).map do |stanza|
+        # list names can include '-': empire-db
+        list = stanza.match(/\/([-\w]*\.?apache\.org)\/(.*?)\//)
+
+        ["#{list[2]}@#{list[1]}", stanza.scan(/^(.*@.*)/).flatten]
+      end
+
+      moderators.each do |mail_list, list_moderators|
+        matches = (list_moderators & user_emails)
+        response[:moderates][mail_list] = matches unless matches.empty?
+      end
+      response
+    end
+    # roster/models/committee: for a mail domain, extract lists and their moderators
+    # roster/models/committer:
+    private
+
+    MEMBERS_SUBSCRIPTIONS = '/srv/subscriptions/members'
+
+    BOARD_SUBSCRIPTIONS = '/srv/subscriptions/board'
+    
+    LIST_MODS = '/srv/subscriptions/list-mods'
+
+    LIST_SUBS = '/srv/subscriptions/list-subs'
+
+  end
+end
