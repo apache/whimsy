abc94b6c3bfcb6affccf7a194d40e01ade3e0ad6
Sam Ruby <rubys@intertwingly.net>
2017-07-14 08:36:05 -0400

    GUINEAPIGS += geronimo

--- lib/whimsy/asf/ldap.rb
+++ lib/whimsy/asf/ldap.rb
@@ -1062,7 +1062,7 @@ module ASF
     end
 
     # temp list of projects that have moved over to new project LDAP schema
-    GUINEAPIGS = %w(incubator whimsy jmeter axis mynewt atlas accumulo)
+    GUINEAPIGS = %w(incubator whimsy jmeter axis mynewt atlas accumulo geronimo)
 
     # List of owners for this committee, i.e. people who are members of the
     # committee and have update access.  Data is obtained from LDAP.
