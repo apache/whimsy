7e59dcb2337fdf0eaff91c877d6904dc0397c1c9
Sam Ruby <rubys@intertwingly.net>
2017-08-03 12:21:33 -0400

    add scaffolding useful for testing puppet vhost macros

---
+++ tools/vhosttest.rb
@@ -0,0 +1,27 @@
+#
+# Scaffolding needed to test infrastructure-puppet/modules/vhosts_whimsy/...
+# preprocess_vhosts.rb puppet macro
+#
+
+require 'whimsy/asf'
+
+IP = ASF::Git['infrastructure-puppet']
+
+module Puppet
+  module Parser
+    module Functions
+      def self.newfunction(*args)
+      end
+    end
+  end
+end
+
+require 'yaml'
+require "#{IP}/modules/vhosts_whimsy/lib/puppet/parser/functions/preprocess_vhosts.rb"
+
+facts = YAML.load_file("#{IP}//data/nodes/whimsy-vm3.apache.org.yaml")
+facts = facts['vhosts_whimsy::vhosts::vhosts']['whimsy-vm-443']
+ldap = ASF::LDAP.hosts.sort.first
+
+macros = Puppet::Parser::Functions::ApacheVHostMacros.new(facts, ldap)
+puts macros.result['custom_fragment']
