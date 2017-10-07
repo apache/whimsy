f523c96b82ba2a8b2db1c7d2142c0f910121e5e2
Sam Ruby <rubys@intertwingly.net>
2017-10-07 09:19:32 -0400

    Fix https://issues.apache.org/jira/browse/WHIMSY-146

--- lib/whimsy/asf/themes.rb
+++ lib/whimsy/asf/themes.rb
@@ -87,7 +87,7 @@ class Wunderbar::HtmlMarkup
   def _whimsy_foot
     _div.footer.container_fluid style: 'background-color: #f5f5f5; padding: 10px;' do
       _p.center do
-        _{'Copyright &copy; 2017, the Apache Software Foundation. Licensed under the '}
+        _{"Copyright \u00A9 2017, the Apache Software Foundation. Licensed under the "}
         _a 'Apache License, Version 2.0', rel: 'license', href: 'http://www.apache.org/licenses/LICENSE-2.0'
         _ ' | '
         _a 'Privacy Policy', href: 'https://www.apache.org/foundation/policies/privacy'
