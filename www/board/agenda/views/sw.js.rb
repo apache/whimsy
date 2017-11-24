a5c3a7042a8255f8ec679b57fb4091c0371b193b
Sam Ruby <rubys@intertwingly.net>
2017-11-24 15:58:45 -0500

    cache stylesheets and javascripts

--- www/board/agenda/views/sw.js.rb
+++ www/board/agenda/views/sw.js.rb
@@ -1,11 +1,18 @@
 #
-# A very simple service worker
+# A service worker that bootstraps the board agenda application
 #
-#   *) Replace calls to fetch an agenda page with calls to fetch a bootstrap
+#   *) Replace calls to fetch any agenda page with calls to fetch a bootstrap
 #      page.   This page will reconstruct the page requested using cached
 #      data and then request fresh data.  If the server doesn't respond
 #      with 0.5 seconds or fails, return a cached version of the bootstrap
 #      page.
+#
+#   *) When a bootstrap.html page is loaded, a scan is made for references
+#      to javascripts and stylesheets, and if such a page is not present in
+#      the cache, it is fetched and the results are cached.
+#
+#   *) Requests for javascript and stylesheets are cached and used to
+#      respond to fetches that fail
 # 
 
 timeout = 500
@@ -21,28 +28,58 @@ self.addEventListener :activate do |event|
   event.waitUntil self.clients.claim();
 end
 
+# look for css and js files and ensure that each are cached
+def preload(cache, base, text)
+  pattern = Regexp.new('"[-.\w+/]+\.(css|js)\?\d+"', 'g')
+
+  while (match = pattern.exec(text))
+    path = match[0].slice(1).split('?')[0]
+    request = Request.new(URL.new(path, base))
+    cache.match(request).then do |response|
+      if not response
+        fetch(request).then do |response|
+          cache.put(request, response) if response.ok
+        end
+      end
+    end
+  end
+end
+
 # respond with bootstrap fetch patch
 self.addEventListener :fetch do |event|
   scope = self.registration.scope
   url = event.request.url
   url = url.slice(scope.length) if url.start_with? scope
 
-  if url =~ %r{^\d\d\d\d-\d\d-\d\d/[-\w]*$} and event.request.method == 'GET'
-    return unless event.request.mode == 'navigate'
+  if event.request.method == 'GET'
     return if url.end_with? '/bootstrap.html'
 
+    # determine what url to fetch (if any)
+    if url =~ %r{^\d\d\d\d-\d\d-\d\d/[-\w]*$}
+      # substitute bootstrap.html for html pages
+      return unless event.request.mode == 'navigate'
+      date =  url.split('/')[0]
+      bootstrap = "#{scope}#{date}/bootstrap.html"
+      fetch_request = Request.new(bootstrap, cache: "no-store")
+      cache_request = fetch_request
+    elsif url =~ %r{\.(js|css)\?\d+$}
+      # cache and respond to js and css requests
+      fetch_request = event.request
+      cache_request = Request.new(url.split('?')[0], cache: "no-store")
+    else
+      return
+    end
+
+    # produce response
     event.respondWith(
       Promise.new do |fulfill, reject|
-        date =  url.split('/')[0]
-        bootstrap = "#{scope}#{date}/bootstrap.html"
-        request = Request.new(bootstrap, cache: "no-store")
         timeoutId = nil
 
         caches.open('board/agenda').then do |cache|
           # common logic to reply from cache
           replyFromCache = lambda do
             timeoutId = nil
-            cache.match(request).then do |response|
+            cache.match(cache_request).then do |response|
               if response
                 fulfill response
               else
@@ -54,11 +91,18 @@ self.addEventListener :fetch do |event|
           # respond from cache if the server isn't fast enough
           timeoutId = setTimeout(replyFromCache, timeout)
 
-          # fetch bootstrap.html
-          fetch(request).then {|response|
+          # fetch bootstrap.html or stylesheet or javascript
+          fetch(fetch_request).then {|response|
             # cache the response if OK, fulfill the response if not timed out
             if response.ok
-              cache.put(request, response.clone())
+              cache.put(cache_request, response.clone())
+
+              if fetch_request.url =~ /bootstrap\.html$/
+                response.clone().text().then do |text|
+                  preload(cache, fetch_request.url, text)
+                end
+              end
+
               if timeoutId
                 clearTimeout timeoutId 
                 fulfill response
