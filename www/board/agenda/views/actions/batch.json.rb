08903ca01bcfc90dc70a0bb498bb81e134629101
Sam Ruby <rubys@intertwingly.net>
2017-11-23 20:56:08 -0500

    apply offline updates

---
+++ www/board/agenda/views/actions/batch.json.rb
@@ -0,0 +1,44 @@
+#
+# Batch apply offline updates
+
+Pending.update(env.user, @agenda) do |pending|
+  agenda = Agenda.parse @agenda, :full
+  @initials ||= pending['initials']
+
+  approved = pending['approved']
+  unapproved = pending['unapproved']
+  flagged = pending['flagged']
+  unflagged = pending['unflagged']
+
+  if @pending['approve']
+    @pending['approve'].each do |attach, request|
+      if request == 'approve'
+        unapproved.delete attach
+        approved << attach unless approved.include? attach or
+          agenda.find {|item| item[:attach] == attach and
+            item['approved'].include? @initials}
+      else
+        approved.delete attach
+        unapproved << attach unless unapproved.include? attach or
+          not agenda.find {|item| item[:attach] == attach and
+            item['approved'].include? @initials}
+      end
+    end
+  end
+
+  if @pending['flag']
+    @pending['flag'].each do |attach, request|
+      if request == 'flag'
+	unflagged.delete attach
+	flagged << attach unless flagged.include? attach or
+	  agenda.find {|item| item[:attach] == attach and
+	    Array(item['flagged_by']).include? @initials}
+      else
+	flagged.delete attach
+	unflagged << attach unless unflagged.include? attach or
+	  not agenda.find {|item| item[:attach] == attach and
+	    Array(item['flagged_by']).include? @initials}
+      end
+    end
+  end
+end
--- www/board/agenda/views/buttons/offline.js.rb
+++ www/board/agenda/views/buttons/offline.js.rb
@@ -2,15 +2,53 @@
 # A button that will toggle offline status
 #
 class Offline < Vue
+  def initialize
+    @disabled = false
+  end
+
   def render
     if Server.offline
-      _button.btn.btn_primary 'go online', onClick: click
+      _button.btn.btn_primary 'go online', onClick: click, disabled: @disabled
     else
-      _button.btn.btn_primary 'go offline', onClick: click
+      _button.btn.btn_primary 'go offline', onClick: click, disabled: @disabled
     end
   end
 
   def click(event)
-    Pending.setOffline(!Server.offline)
+    if Server.offline
+      @disabled = true
+
+      Pending.dbget do |pending|
+	# construct arguments to fetch
+	args = {
+	  method: 'post',
+	  credentials: 'include',
+	  headers: {'Content-Type' => 'application/json'},
+	  body: {agenda: Agenda.file, pending: pending}.inspect
+	}
+
+        fetch('../json/batch', args).then {|response|
+          if response.ok
+            Pending.dbput({})
+            response.json().then do |pending| 
+              Server.pending = pending
+            end
+            Pending.setOffline(false)
+          else
+            response.text().then do |text| 
+              alert("Server error: #{response.status}")
+              console.log text
+            end
+          end
+
+          @disabled = false
+        }.catch {|error|
+          alert(error)
+          @disabled = false
+        }
+      end
+    else
+      Pending.setOffline(true)
+    end
   end
 end
