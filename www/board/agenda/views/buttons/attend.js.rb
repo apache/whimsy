96f5a3e8487d3e60a728d31379e2892db3d7ee9c
Sam Ruby <rubys@intertwingly.net>
2017-12-20 11:17:39 -0500

    update based on client model changes

--- www/board/agenda/views/buttons/attend.js.rb
+++ www/board/agenda/views/buttons/attend.js.rb
@@ -13,14 +13,14 @@ class Attend < Vue
 
   # match person by either userid or name
   def created()
-    person = @@item.people[User.userid]
+    person = @@item.people[User.id]
     if person
       @attending = person.attending
     else
       @attending = false
       for id in @@item.people
         person = @@item.people[id]
-        @attending = person.attending if person.name == Pending.username
+        @attending = person.attending if person.name == User.username
       end
     end
   end
@@ -29,8 +29,8 @@ class Attend < Vue
     data = {
       agenda: Agenda.file,
       action: (@attending ? 'regrets' : 'attend'),
-      name: Pending.username,
-      userid: User.userid
+      name: User.username,
+      userid: User.id
     }
 
     @disabled = true
