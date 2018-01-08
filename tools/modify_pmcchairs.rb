4ded0cba211d7ddef649a4ecadc851fd80e75f8c
Craig L Russell <craig.russell@oracle.com>
2018-01-08 15:58:33 -0800

    Fix typo -rm instead of --rm; only remove from LDAP if not in committee-info

--- tools/modify_pmcchairs.rb
+++ tools/modify_pmcchairs.rb
@@ -24,11 +24,16 @@ end
 # get the list from LDAP to be updated
 chairs = ASF::Service.find('pmc-chairs')
 
+# get the list of current chairs from committee-info
+current = ASF::Committee.pmcs.map(&:chair).uniq
+# only remove people from LDAP who are not currently chairs
+to_be_removed = people.reject{|p| current.include?(p)}
+
 # execute the action
 if action == '--add' and not people.empty?
   ASF::LDAP.bind { chairs.add(people) }
-elsif action == '-rm' and not people.empty?
-  ASF::LDAP.bind { chairs.remove(people) }
+elsif action == '--rm' and not to_be_removed.empty?
+  ASF::LDAP.bind { chairs.remove(to_be_removed) }
 else
   STDERR.puts "Usage: #{$PROGRAM_NAME} (--add|--rm) list..."
 end
