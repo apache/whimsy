1eb39604afa44c28f352a58c8f27803ba9694327
Sam Ruby <rubys@intertwingly.net>
2018-04-14 21:47:26 -0400

    fix regression on legal affairs report display

--- www/board/agenda/views/models/agenda.js.rb
+++ www/board/agenda/views/models/agenda.js.rb
@@ -452,10 +452,10 @@ class Agenda
 
     if @attach =~ /^(\d|7?[A-Z]+|4[A-Z]|8[.A-Z])$/
       if User.role == :secretary or not Minutes.complete
-        if @attach.start_with? '8'
+        if @attach =~ /^8[.A-Z]/
           if @attach =~ /^8[A-Z]/
             list << {form: Post, text: 'edit item'}
-          elsif @text.strip().empty?
+          elsif not text or @text.strip().empty?
             list << {form: Post, text: 'post item'}
           else
             list << {form: Post, text: 'edit items'}
