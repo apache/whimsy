59cc400ca7172a5c30f4c27b02cfe6a869b5f79b
Sam Ruby <rubys@intertwingly.net>
2018-04-24 10:00:15 -0400

    once meeting starts, consider missing reports and reports without sufficient preapprovals as flagged

--- www/board/agenda/views/pages/flagged.js.rb
+++ www/board/agenda/views/pages/flagged.js.rb
@@ -7,7 +7,13 @@ class Flagged < Vue
     first = true
 
     Agenda.index.each do |item|
-      if item.flagged_by or Pending.flagged.include? item.attach
+      flagged = item.flagged_by or Pending.flagged.include? item.attach
+
+      if not flagged and Minutes.started and item.attach =~ /^(\d+|[A-Z]+)$/
+        flagged = !item.skippable
+      end
+
+      if flagged
         _h3 class: item.color do
           _Link text: item.title, href: "flagged/#{item.href}",
             class: ('default' if first)
@@ -16,7 +22,7 @@ class Flagged < Vue
           _span.owner " [#{item.owner} / #{item.shepherd}]"
 
           flagged_by = Server.directors[item.flagged_by] || item.flagged_by
-          _span.owner " flagged by: #{flagged_by}"
+          _span.owner " flagged by: #{flagged_by}" if flagged_by
         end
 
         _AdditionalInfo item: item, prefix: true
