56ea35d6e8ca085632cb3c39c4068a3e7a174cbd
Sam Ruby <rubys@intertwingly.net>
2018-05-23 13:02:39 -0400

    conditionally define new method

--- www/board/agenda/views/committers_report.text.rb
+++ www/board/agenda/views/committers_report.text.rb
@@ -3,14 +3,16 @@ require 'chronic'
 ## This is a script to generate an email for committers@apache.org
 
 # Add the right prefix to a number
-class Integer
-  def ordinalize
-    if self % 10 == 1
-      self.to_s + "st"
-    elsif self % 10 == 2
-      self.to_s + "nd"
-    else
-      self.to_s + "th"
+unless Integer.public_method_defined? :ordinalize
+  class Integer
+    def ordinalize
+      if self % 10 == 1
+	self.to_s + "st"
+      elsif self % 10 == 2
+	self.to_s + "nd"
+      else
+	self.to_s + "th"
+      end
     end
   end
 end
