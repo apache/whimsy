0e1b8adc07fc291956111248ddc731282f972b62
Sam Ruby <rubys@intertwingly.net>
2018-06-04 09:13:55 -0400

    add some tests

---
+++ www/board/agenda/spec/reflow_spec.rb
@@ -0,0 +1,41 @@
+#
+# component tests for client side forms
+#
+
+require_relative 'spec_helper'
+require_relative 'vue_server'
+
+describe "reflow", type: :feature, server: :vue do
+  #
+  # Comment form
+  #
+  describe "incubator rules" do
+    it "handles ordered lists" do
+      on_vue_server do
+        class TestReflow < Vue
+          def render
+            _ Flow.text("1. a\n2. b")
+          end
+        end
+
+        Vue.renderResponse(TestReflow, response)
+      end
+
+      expect(page.body).to eq "1. a\n2. b"
+    end
+
+    it "handles questions and colons" do
+      on_vue_server do
+        class TestReflow < Vue
+          def render
+            _ Flow.text("a?\nb:\nc", '', true)
+          end
+        end
+
+        Vue.renderResponse(TestReflow, response)
+      end
+
+      expect(page.body).to eq "a?\nb:\nc"
+    end
+  end
+end
