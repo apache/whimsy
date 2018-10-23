005c60e938132260d9e408fbd1f12cc8dbee0ca3
Sebb <sebb@apache.org>
2018-10-23 18:35:11 +0100

    Detect concatenation failure

--- www/secretary/workbench/views/actions/drop.json.rb
+++ www/secretary/workbench/views/actions/drop.json.rb
@@ -8,18 +8,24 @@ begin
   source = message.find(@source).as_pdf
   target = message.find(@target).as_pdf
 
-  output = SafeTempFile.new('output')
+  output = SafeTempFile.new('output') # N.B. this is created as binary
 
   Kernel.system 'pdftk', target.path, source.path, 'cat', 'output',
     output.path
 
   name = @target.sub(/\.\w+$/, '') + '.pdf'
 
+  # If output file is empty, then the command failed
+  raise "Failed to concatenate #{@target} and #{@source}" unless File.size? output
+
   message.update_attachment @target, content: output.read, name: name,
     mime: 'application/pdf'
 
   message.delete_attachment @source
 
+rescue
+  Wunderbar.error "Failed to concatenate #{@target} and #{@source}"
+  raise
 ensure
   source.unlink if source
   target.unlink if target
