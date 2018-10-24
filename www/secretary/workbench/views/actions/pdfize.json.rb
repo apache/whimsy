652c8bcb3ae5e513d842624b0c47fa411979b9b6
Sebb <sebb@apache.org>
2018-10-24 10:45:00 +0100

    Detect conversion failure

--- www/secretary/workbench/views/actions/pdfize.json.rb
+++ www/secretary/workbench/views/actions/pdfize.json.rb
@@ -12,6 +12,9 @@ begin
 
   name = @selected.sub(/\.\w+$/, '') + '.pdf'
 
+  # If output file is empty, then the command failed
+  raise "Failed to pdf-ize #{@selected} in #{@message}" unless File.size? source.path
+
   message.update_attachment @selected, content: source.read, name: name,
     mime: 'application/pdf'
 
