763527ff9b70d9cf1674094bb0d89440dcfd0b72
Sebb <sebb@apache.org>
2019-01-07 11:59:51 +0000

    Simplify as source files are in SVN

--- tools/toccomments.sh
+++ tools/toccomments.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
 # Fix incorrectly wrapped comments in Incubator ToC section
+# Intended for use on archived agendas and published minutes.
 
 # Look for Comments: preceeded by non-space
-ruby -p -i.orig -e 'gsub(/(\S)\s+(Comments:)/,"\\1\n     \\2")' "$@"
-for i in "$@"
-do
-  # if the files are the same, drop the unchanged output
-  if cmp -s $i $i.orig
-  then
-      echo "$i has not changed"
-      rm $i.orig
-  else
-      echo "$i has changed"
-#       ls -l $i*
-  fi
-done
+ruby -p -i -e 'gsub(/(\S)\s+(Comments:)/,"\\1\n     \\2")' "$@"
+# no need to save original files as tool is intended for use with files in SVN/Git
+echo "Done; the updated files can be diffed/checked in as required"
