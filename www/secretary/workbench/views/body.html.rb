04741197882b5d44d8020f5ee03c6c011680f660
Sebb <sebb@apache.org>
2019-01-08 16:37:28 +0000

    Handle non-multipart mails

--- www/secretary/workbench/views/body.html.rb
+++ www/secretary/workbench/views/body.html.rb
@@ -58,5 +58,27 @@ _html do
     end
 
     _pre.bg_info body.encode('utf-8', invalid: :replace, undef: :replace)
+  else # must be a non-multipart message
+    body_part = @message.mail.body
+    body = body_part.to_s
+    mime_type = @message.mail.mime_type
+
+    _.comment! "body.encoding=#{body.encoding} body_part.charset=#{body_part.charset} mime_type=#{mime_type}"
+
+    if body.encoding == Encoding::BINARY and body_part.charset
+      body.force_encoding(body_part.charset) rescue nil
+    end
+
+    if mime_type == 'text/plain'
+      _pre.bg_info body.encode('utf-8', invalid: :replace, undef: :replace)
+    elsif mime_type == 'text/html'
+      _div do
+        nodes = _{body.encode('utf-8', invalid: :replace, undef: :replace)}
+
+        fixup_images(nodes)
+      end
+    else
+      _p "(Cannot handle mime-type #{mime_type})"
+    end
   end
 end
