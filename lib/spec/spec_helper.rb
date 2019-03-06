bbca23dbae64d5bef5cf9dc05ac9b134555a4559
Sebb <sebb@apache.org>
2019-03-06 00:18:48 +0000

    Allow access to fixtures

--- lib/spec/spec_helper.rb
+++ lib/spec/spec_helper.rb
@@ -1,1 +1,9 @@
 $LOAD_PATH.unshift '/srv/whimsy/lib'
+
+unless defined?(SPEC_ROOT)
+  SPEC_ROOT = File.join(File.dirname(__FILE__))
+end
+
+def fixture_path(*path)
+  File.join SPEC_ROOT, 'fixtures', path
+end
