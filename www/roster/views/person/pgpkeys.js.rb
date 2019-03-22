9f261bd03b0950c66c994c2c75277de72e3e84d3
Sebb <sebb@apache.org>
2019-03-22 12:48:19 +0000

    Flag short PGP keys

--- www/roster/views/person/pgpkeys.js.rb
+++ www/roster/views/person/pgpkeys.js.rb
@@ -13,12 +13,19 @@ class PersonPgpKeys < Vue
         _ul committer.pgp do |key|
           _li do
             if key =~ /^[0-9a-fA-F ]+$/
+              k = key.gsub(' ', '')
               _samp do
                 _a key, target: '_blank', href: 'https://sks-keyservers.net/pks/lookup?' +
-                  'op=index&fingerprint=on&hash=on&search=0x' + key.gsub(' ', '')
+                  'op=index&fingerprint=on&hash=on&search=0x' + k
+                unless k.length == 40
+                  _span.bg_danger ' ?? Expecting exactly 40 hex characters (plus optional spaces)'
+                end
               end
             else
-              _samp key
+              _samp do
+                _ key
+                _span.bg_danger ' ?? Expecting exactly 40 hex characters (plus optional spaces)'
+              end
             end
           end
         end
