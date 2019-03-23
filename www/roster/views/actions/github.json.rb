81cea45d3d314ca8d4ec60eeecdd596be3423d82
Sebb <sebb@apache.org>
2019-03-23 10:18:19 +0000

    Don't bind to LDAP for a dry-run (or no data)

--- www/roster/views/actions/github.json.rb
+++ www/roster/views/actions/github.json.rb
@@ -2,18 +2,18 @@
 # Update GitHub username attribute for a committer
 #
 
-# update LDAP
-_ldap.update do
-  person = ASF::Person.find(@userid)
+person = ASF::Person.find(@userid)
 
-  # report the previous value in the response
-  _previous githubUsername: person.attrs['githubUsername']
+# report the previous value in the response
+_previous githubUsername: person.attrs['githubUsername']
 
-  if @githubuser and not @dryrun
-    # TODO: Hack to deal with single input field on the form
-    # multiple entries are currently displayed with commas when editting
-    names = @githubuser.split(/[, ]+/).uniq{|n| n.downcase} # duplicates not allowed; case-blind
-    person.modify 'githubUsername', names
+if @githubuser and not @dryrun
+  # TODO: Hack to deal with single input field on the form
+  # multiple entries are currently displayed with commas when editting
+  names = @githubuser.split(/[, ]+/).uniq{|n| n.downcase} # duplicates not allowed; case-blind
+  # update LDAP
+  _ldap.update do
+     person.modify 'githubUsername', names
   end
 end
 
