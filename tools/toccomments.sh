1cb70140cf2344c3b8020d2c51db882b6180eee2
Sebb <sebb@apache.org>
2019-04-18 10:46:10 +0100

    Only fix lines that appear to be in the correct section

--- tools/toccomments.sh
+++ tools/toccomments.sh
@@ -3,7 +3,8 @@
 # Fix incorrectly wrapped comments in Incubator ToC section
 # Intended for use on archived agendas and published minutes.
 
-# Look for Comments: preceeded by non-space
-ruby -p -i -e 'gsub(/(\S)\s+(Comments:)/,"\\1\n     \\2")' "$@"
+# Look for Comments: preceeded by non-space if line contains e.g.  [ ](batchee)
+# N.B. there are some minutes with '[](...)' and some with '[ ] (..)'
+ruby -p -i -e 'gsub(/(\S)\s+(Comments:)/,"\\1\n     \\2") if /^ +\[.?\] ?\(\S+\) /' "$@"
 # no need to save original files as tool is intended for use with files in SVN/Git
 echo "Done; the updated files can be diffed/checked in as required"
