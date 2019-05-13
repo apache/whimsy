38871d046260dc53ffceef080c1afd21aaaf1587
Sam Ruby <rubys@intertwingly.net>
2019-05-12 22:49:46 -0400

    rough in an empty svn test directory
    Now all that is needed is some test data.
    
    Sebb: Feel free to adjust the name of the directory as you see fit.
    
    Note: since the directory in question is read-only, checking in dummy
    data into git should be sufficient.  In the event that updating of svn
    data is needed for a test, see the calls to `svnadmin` in
    whimsy/board/agenda/Rakefile to see how a local repository can be created,
    populated with data, and have a working directory checked out locally.

--- lib/spec/spec_helper.rb
+++ lib/spec/spec_helper.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift '/srv/whimsy/lib'
 
+require 'whimsy/asf'
+ASF::SVN['apmail_bin'] = File.expand_path('../test/svn/apmail_bin', __dir__)
+
 unless defined?(SPEC_ROOT)
   SPEC_ROOT = File.join(File.dirname(__FILE__))
 end
