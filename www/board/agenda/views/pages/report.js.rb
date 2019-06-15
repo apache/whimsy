8e4e271b9241c80e85f17c0bc37d861ef7b5c37b
Sam Ruby <rubys@intertwingly.net>
2019-06-15 16:23:34 -0400

    mark privates in resolutions

--- www/board/agenda/views/pages/report.js.rb
+++ www/board/agenda/views/pages/report.js.rb
@@ -72,7 +72,7 @@ class Report < Vue
     list << self.names if @@item.people
     list << self.president_attachments if @@item.title == 'President'
 
-    list = [self.linkMinutes] if @@item.attach =~ /^[37][A-Z]$/
+    list = [self.privates, self.linkMinutes] if @@item.attach =~ /^[37][A-Z]$/
 
     list
   end
