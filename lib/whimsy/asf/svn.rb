4be7c29a7463b95ec7468e4e7fdedcc22a826408
Sebb <sebb@apache.org>
2019-07-17 23:40:18 +0100

    Revert "WHIMSY-278 Update the local working copy"
    This reverts commit 8ded5d617af2f6d9dcb4e48f9adc5c0e8a2049fd.

--- lib/whimsy/asf/svn.rb
+++ lib/whimsy/asf/svn.rb
@@ -336,10 +336,6 @@ module ASF
       ensure
         FileUtils.rm_rf tmpdir
       end
-      # Now update the local working copy
-      _.system ['svn', 'update', '--non-interactive',
-        ['--username', env.user, '--password', env.password],
-        dir]
     end
   end
 
