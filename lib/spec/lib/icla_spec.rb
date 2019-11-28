446cf4c8c5eac143679a3dc9f9391124fc682c28
Sebb <sebb@apache.org>
2019-11-28 21:30:34 +0000

    Doh! Git does not like empty directories

--- lib/spec/lib/icla_spec.rb
+++ lib/spec/lib/icla_spec.rb
@@ -11,14 +11,7 @@ set_svn 'iclas' # only works with test data as real entry names should not be pu
 # ab/        abc.pdf    abcd/      abcd.pdf   abcde/
 
 describe ASF::ICLAFiles do
-  #+ Debug for Travis
-  dir = ASF::SVN['iclas']
-  puts dir
-  abcl = Dir[File.join(dir,'abc*')].length
-  puts abcl
-  condition = abcl == 4
-  #- Debug 
-  describe "ASF::ICLAFiles.match_claRef", :if => condition do
+  describe "ASF::ICLAFiles.match_claRef" do
     it "should return nil for 'xyz'" do
       res = ASF::ICLAFiles.match_claRef('xyz')
       expect(res).to equal(nil)
