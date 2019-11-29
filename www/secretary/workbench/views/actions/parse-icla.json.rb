4c1a7891111cc1b20e604373c8d2b4903718014e
Sebb <sebb@apache.org>
2019-11-29 10:41:09 +0000

    Need to decode URI e.g. for encoded spaces

--- www/secretary/workbench/views/actions/parse-icla.json.rb
+++ www/secretary/workbench/views/actions/parse-icla.json.rb
@@ -3,10 +3,12 @@
 require_relative '../../../iclaparser'
 require_relative '../../models/mailbox'
 
-if @attachment.end_with? '.pdf'
+attachment = URI.decode(@attachment) # derived from a URI
+
+if attachment.end_with? '.pdf'
   message = Mailbox.find(@message)
   
-  path = message.find(@attachment).as_file.path
+  path = message.find(attachment).as_file.path
   
   parsed = ICLAParser.parse(path)
 else
