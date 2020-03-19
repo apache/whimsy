48540a8b54ed604c4be8aac132921e4b493050c0
Sebb <sebb@apache.org>
2020-03-19 16:42:28 +0000

    Typo

--- www/roster/models/committee.rb
+++ www/roster/models/committee.rb
@@ -20,7 +20,7 @@ class Committee
     info = JSON.parse(File.read(File.join(comdev, 'projects.json')))[id]
 
     image_dir = ASF::SVN.find('site-img')
-    image = image_dir? Dir[File.join(image_dir, "#{id}*.{svg,eps,ai,pdf}")].map {|path| File.basename(path)}.first : nil
+    image = image_dir ? Dir[File.join(image_dir, "#{id}*.{svg,eps,ai,pdf}")].map {|path| File.basename(path)}.first : nil
 
     moderators = nil
     modtime = nil
