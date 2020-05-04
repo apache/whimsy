0c82906705c65f5d14a1ac479185e4a88db249e0
Sebb <sebb@apache.org>
2020-05-04 16:50:56 +0100

    Speed up matchStem and match_claRef
    Turns out that repeated selects are rather slow if combined
    with modification of values, so create hash with results

--- lib/whimsy/asf/documents.rb
+++ lib/whimsy/asf/documents.rb
@@ -42,17 +42,34 @@ module ASF
   class ICLAFiles
     @@tag = nil # probably worth caching iclas
     @@list = nil # this list includes trailing '/' so can detect directories correctly
+    # Turns out that select{|l| ! l.end_with?('/') && l.start_with?("#{stem}.")} is quite slow,
+    # so create hashes from the list
+    @@h_claRef = nil # for matching claRefs
+    @@h_stem = nil # for matching stems
 
     # search icla files to find match with claRef
     # matches if the input matches the full name of a file or directory or 
     # it matches with an extension
     # Returns the basename or nil if no match
     def self.match_claRef(claRef)
-      # Match either full name (e.g. directory) or stem (e.g. name.pdf)
-      listnames.select{|l| l.chomp('/') == claRef || l.start_with?("#{claRef}.") }.map {|m| m.chomp('/')}.first
+      unless @@h_claRef
+        h_claRef = Hash.new
+        listnames.map do |l|
+          # Match either full name (e.g. directory) or stem (e.g. name.pdf)
+          if l.end_with? '/'
+            h_claRef[l.chomp('/')] = l.chomp('/')
+          elsif l.include?('.')
+            h_claRef[l.split('.')[0]] = l
+          else
+            h_claRef[l] = l
+          end
+        end
+        @@h_claRef = h_claRef
+      end
+      @@h_claRef[claRef]
     end
 
-    # is the name a directory?
+# is the name a directory?
     def self.Dir?(name)
       listnames.include? name + '/'
     end
@@ -60,15 +77,26 @@ module ASF
     # return a list of names matching stem.*
     # Does not return directories
     def self.matchStem(stem)
-    listnames.select{|l| ! l.end_with?('/') && l.start_with?("#{stem}.")}
+      unless @@h_stem
+        h_stem = Hash.new{|h,k| h[k] = []}
+          listnames.map do |l|
+            h_stem[l.split('.')[0]] << l unless l.end_with?('/')
+          end
+        @@h_stem = h_stem
+      end
+      @@h_stem[stem]
     end
 
     # This returns the list of names in the top-level directory
     # directory names are terminated by '/'
     def self.listnames
       @@tag, list = ASF::SVN.getlisting('iclas', @@tag, false)
-      if list
+      if list # we have a new list
+        # update the list cache
         @@list = list
+        # clear the hash caches
+        @@h_claRef = nil
+        @@h_stem = nil
       end
       @@list      
     end
