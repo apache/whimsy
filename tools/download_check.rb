8f8f80dd7f55f892ecc8858d172c190a79b57d7b
Sebb <sebb@apache.org>
2020-05-20 12:00:19 +0100

    Allow for SHA as generic hash indicator

--- tools/download_check.rb
+++ tools/download_check.rb
@@ -462,6 +462,7 @@ def _checkDownloadPage(path, tlp, version)
         tmp = text2ext(t)
         next if ext == tmp # i.e. link is just the type or [TYPE]
         next if ext == 'sha' and tmp == 'sha1' # historic
+        next if (ext == 'sha256' or ext == 'sha512') and t == 'SHA' # generic
         if not base == t and not t == 'checksum'
             E "Mismatch: #{h} and '#{t}'"
         end
