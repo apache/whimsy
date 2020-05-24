b1be993457fe6501011bdbb886bee1060cf25665
Sebb <sebb@apache.org>
2020-05-24 14:38:05 +0100

    List role groups

---
+++ www/roster/public_ldap_roles.rb
@@ -0,0 +1,61 @@
+# Creates JSON output with the following format:
+#
+# {
+#   "lastTimestamp": "20160119171152Z", // most recent modifyTimestamp
+#   "rolegroups": {
+#     "committers": {
+#       "modifyTimestamp": "20111204095436Z",
+#       "roster": ["uid",
+#       ...
+#       ]
+#     },
+#     ...
+#   },
+# }
+#
+
+require_relative 'public_json_common'
+
+# gather service group info
+entries = {}
+
+groups = ASF::RoleGroup.preload # for performance
+
+if groups.empty?
+  Wunderbar.error "No results retrieved, output not created"
+  exit 0
+end
+
+lastStamp = ''
+groups.keys.sort_by {|a| a.name}.each do |entry|
+    next if entry.name == 'apldap' # infra team would prefer this not be publicized
+
+    m = []
+    entry.members.sort_by {|a| a.name}.each do |e|
+        m << e.name
+    end
+    lastStamp = entry.modifyTimestamp if entry.modifyTimestamp > lastStamp
+    entries[entry.name] = {
+        createTimestamp: entry.createTimestamp,
+        modifyTimestamp: entry.modifyTimestamp,
+        roster: m 
+    }
+end
+
+info = {
+  # Is there a use case for the last createTimestamp ?
+  lastTimestamp: lastStamp,
+  rolegroups: entries,
+}
+
+public_json_output(info)
+
+if changed? and @old_file
+  # for validating UIDs
+  uids = ASF::Person.list().map(&:id)
+  entries.each do |name, entry|
+    entry[:roster].each do |id|
+      Wunderbar.warn "#{name}: unknown uid '#{id}'" unless uids.include?(id)
+    end
+  end
+end
