0b19d6fe90e749e1d3ec326cc029943ce6c8d818
Craig L Russell <apache.clr@gmail.com>
2020-06-29 15:15:49 -0700

    remove debug messages

--- www/roster/views/actions/memstat.json.rb
+++ www/roster/views/actions/memstat.json.rb
@@ -1,13 +1,8 @@
 # get entry for @userid
 require 'wunderbar'
-Wunderbar.warn('Memstat.json.rb action: ' + @action +
-               ' for id: ' + @userid)
-Wunderbar.warn('Memstat.json.rb request with emeritusfilename: ' + @emeritusfilename) if @emeritusfilename
-Wunderbar.warn('Memstat.json.rb request with emerituspersonname: ' + @emerituspersonname) if @emerituspersonname
-
 user = ASF::Person.find(@userid)
 entry = user.members_txt(true)
-raise Exception.new("unable to find member entry for #{userid}") unless entry
+raise Exception.new("Unable to find member entry for #{@userid}") unless entry
 USERID = user.id.untaint
 USERMAIL = "#{USERID}@apache.org".untaint
 USERNAME = user.cn.untaint
@@ -17,67 +12,66 @@ TIMESTAMP = (DateTime.now.strftime "%Y-%m-%d %H:%M:%S").untaint
 members_txt = File.join(ASF::SVN['foundation'], 'members.txt').untaint
 
 # construct commit message
-message = "Action #{@action} for #{ASF::Person.find(@userid).member_name}"
+message = "Action #{@action} for #{USERID}"
 
 # only update members if needed
 updmem = @action == 'emeritus' or @action == 'active' or @action == 'deceased'
 
 # update members.txt only for secretary actions
-ASF::SVN.multiUpdate members_txt, message, env, _ do |text|
-  # default command is empty
-  command = ""
-  # remove user's entry
-  unless text.sub! entry, '' # e.g. if the workspace was out of date
-    raise Exception.new("Failed to remove existing entry -- try refreshing")
-  end
-
-  # determine where to put the entry
-  if @action == 'emeritus'
-    index = text.index(/^\s\*\)\s/, text.index(/^Emeritus/))
-    entry.sub! %r{\s*/\* deceased, .+?\*/},'' # drop the deceased comment if necessary
-    # if pending emeritus request was found, move it to emeritus
-    extra = []
-    if @emeritusfilename.index('/emeritus-requests-received/')
-      emeritus_url = ASF::SVN.svnurl('emeritus')
-      command = "svn move #{@emeritusfilename} #{emeritus_url}"
-      Wunderbar.warn "memstat.json.rb action emeritus commmand: #{command}"
-      extra << ['move', @emeritusfilename, emeritus_url]
+if updmem
+  ASF::SVN.multiUpdate_ members_txt, message, env, _ do |text|
+    # default command is empty
+    command = ""
+    # remove user's entry
+    unless text.sub! entry, '' # e.g. if the workspace was out of date
+      raise Exception.new("Failed to remove existing entry -- try refreshing")
     end
-  elsif @action == 'active'
-    index = text.index(/^\s\*\)\s/, text.index(/^Active/))
-    entry.sub! %r{\s*/\* deceased, .+?\*/},'' # drop the deceased comment if necessary
-    # if emeritus file was found, move it to emeritus-reinstated
-    if @emeritusfilename.index('//emeritus//')
-      emeritus_reinstated_url = ASF::SVN.svnurl('emeritus-reinstated')
-      command = "svn move #{@emeritusfilename} #{emeritus_reinstated_url}"
-      Wunderbar.warn "memstat.json.rb action emeritus commmand: #{command}"
-      extra << ['move', @emeritusfilename, emeritus_reinstated_url]
+
+    # determine where to put the entry
+    if @action == 'emeritus'
+      index = text.index(/^\s\*\)\s/, text.index(/^Emeritus/))
+      entry.sub! %r{\s*/\* deceased, .+?\*/},'' # drop the deceased comment if necessary
+      # if pending emeritus request was found, move it to emeritus
+      extra = []
+      if @emeritusfilename.index('/emeritus-requests-received/')
+        emeritus_url = ASF::SVN.svnurl('emeritus')
+        command = "svn move #{@emeritusfilename} #{emeritus_url}"
+        Wunderbar.warn("memstat.json.rb emeritus with command: #{command}")
+        extra << ['move', @emeritusfilename, emeritus_url]
+      end
+    elsif @action == 'active'
+      index = text.index(/^\s\*\)\s/, text.index(/^Active/))
+      entry.sub! %r{\s*/\* deceased, .+?\*/},'' # drop the deceased comment if necessary
+      # if emeritus file was found, move it to emeritus-reinstated
+      if @emeritusfilename.index('//emeritus//')
+        emeritus_reinstated_url = ASF::SVN.svnurl('emeritus-reinstated')
+        command = "svn move #{@emeritusfilename} #{emeritus_reinstated_url}"
+        extra << ['move', @emeritusfilename, emeritus_reinstated_url]
+      end
+    elsif @action == 'deceased'
+      index = text.index(/^\s\*\)\s/, text.index(/^Deceased/))
+      entry.sub! %r{\n}, " /* deceased, #{@dod} */\n" # add the deceased comment
     end
-  elsif @action == 'deceased'
-    index = text.index(/^\s\*\)\s/, text.index(/^Deceased/))
-    entry.sub! %r{\n}, " /* deceased, #{@dod} */\n" # add the deceased comment
+
+    # perform the insertion
+    text.insert index, entry
+
+    # return the updated (and normalized) text and extra svn command
+    [text, extra]
   end
+end
 
-  # perform the insertion
-  text.insert index, entry
-
-  # return the updated (and normalized) text and extra svn command
-  [text, extra]
-end if updmem #only update members.txt for secretary actions
-
+# Owner operations
 if @action == 'rescind_emeritus'
   emeritus_rescinded_url = ASF::SVN.svnurl('emeritus-requests-rescinded')
   pw = " --password #{env.password}" if env.password
   credentials = "--username #{env.user}#{pw}"
   command = "svn move #{credentials} -m \"#{message}\" #{@emeritusfilename} #{emeritus_rescinded_url}"
   _.system command
-  Wunderbar.warn ("memstat.json.rb rescind_emeritus command: #{command}")
 elsif @action == 'request_emeritus'
   # Create mail to secretary requesting emeritus
   FOUNDATION_URL = ASF::SVN.svnurl('foundation')
-  Wunderbar.warn("Memstat.json.rb foundation url: #{FOUNDATION_URL}")
   EMERITUS_TEMPLATE_URL = File.join(FOUNDATION_URL, 'emeritus-request.txt').untaint
-  Wunderbar.warn("Memstat.json.rb emeritus template url: #{EMERITUS_TEMPLATE_URL}")
   template, err =
     ASF::SVN.svn('cat', EMERITUS_TEMPLATE_URL, {user: $USER.dup.untaint, password: $PASSWORD.dup.untaint})
   raise RuntimeError.new("Failed to read emeritus-request.txt: " + err) unless template
@@ -107,7 +101,6 @@ elsif @action == 'request_emeritus'
   mail.attachments["#{USERID}.txt"] = signed_request.untaint
   mail.deliver!
 elsif @action == 'request_reinstatement'
-  Wunderbar.warn "memstat.json.rb request reinstatement"
   ASF::Mail.configure
   mail = Mail.new do
     to "secretary@apache.org"
