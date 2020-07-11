520fe46e7055f1699b4c1f2b4edced729a191e81
Sebb <sebb@apache.org>
2020-07-11 13:36:50 +0100

    ensure file url is actually passed back

--- www/roster/views/person/memstat.js.rb
+++ www/roster/views/person/memstat.js.rb
@@ -19,19 +19,19 @@ class PersonMemberStatus < Vue
             _form.inline method: 'post' do
               # Cancel this form (implemented in main.js.rb submit(event)
               _button.btn.btn_secondary 'Cancel', data_cancel_submit:true
-
+              emeritus_file_url = nil # for return by hidden form item
               # These actions are only for the person's own use
               if owner
                 if committer.member.status.include? 'Active'
                   if committer.forms['emeritus_request']
                     emeritus_file_url = committer.forms['emeritus_request']
                     _button.btn.btn_primary 'rescind emeritus request',
-                      data_emeritus_file_url:emeritus_file_url,
                       name: 'action', value: 'rescind_emeritus'
                   else
-                    _button.btn.btn_primary 'request emeritus status',
-                      data_emeritus_person_name:@@person.public_name,
-                      name: 'action', value: 'request_emeritus'
+                    unless committer.forms['emeritus_request'] # already requested
+                      _button.btn.btn_primary 'request emeritus status',
+                        name: 'action', value: 'request_emeritus'
+                    end
                   end
                 elsif committer.member.status.include? 'Emeritus'
                   _button.btn.btn_primary 'request reinstatement',
@@ -40,12 +40,11 @@ class PersonMemberStatus < Vue
               end
               # These actions are only for secretary's use
               if @@person.props.auth.secretary
-                console.log('memstat edit menu secretary...')
                 if committer.member.status.include? 'Active'
                   emeritus_file_url = committer.forms['emeritus_request']
+                  # TODO check time has expired
                   if emeritus_file_url
                     _button.btn.btn_primary 'move to emeritus',
-                      data_emeritus_file_url:emeritus_file_url,
                       name: 'action', value: 'emeritus'
                   end
                   _button.btn.btn_primary 'move to deceased',
@@ -54,7 +53,6 @@ class PersonMemberStatus < Vue
                 elsif committer.member.status.include? 'Emeritus'
                   emeritus_file_url = committer.forms['emeritus']
                   _button.btn.btn_primary 'move to active',
-                    data_emeritus_file_url:emeritus_file_url,
                     name: 'action', value: 'active'
                   _button.btn.btn_primary 'move to deceased',
                     name: 'action', value: 'deceased'
@@ -62,11 +60,14 @@ class PersonMemberStatus < Vue
                 elsif committer.member.status.include? 'Deceased'
                   _button.btn.btn_primary 'move to active',
                     name: 'action', value: 'active'
+                  # TODO: what about the emeritus file?
                   _button.btn.btn_primary 'move to emeritus',
                     name: 'action', value: 'emeritus'
                 end
               end
-            end
+              # Pass back the file url
+              _input type: 'hidden', name: 'emeritus_file_url', value: emeritus_file_url
+            end # end _form
           end
         end
       end
