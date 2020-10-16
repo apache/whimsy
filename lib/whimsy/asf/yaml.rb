98819fd45e9348235ef968259f31a2f5b262d7bf
Sebb <sebb@apache.org>
2020-10-16 13:10:28 +0100

    Enable Symbols; don't suppress errors

--- lib/whimsy/asf/yaml.rb
+++ lib/whimsy/asf/yaml.rb
@@ -10,13 +10,16 @@ module YamlFile
 
   #
   # encapsulate updates to a YAML file
-  # opens the file for exclusive access with an exclusive lock
+  # opens the file for exclusive access with an exclusive lock,
+  # creating the file if necessary
   # Yields the parsed YAML to the block, and writes the updated
   # data to the file
+  # The args are passed to YAML.safe_load, and default to [Symbol]
   def self.update(yaml_file, *args)
-    File.open(yaml_file, File::RDWR|File::CREAT, 0644) do |file|
+    args << [Symbol] if args.empty?
+    File.open(yaml_file, File::RDWR|File::CREAT, 0o644) do |file|
       file.flock(File::LOCK_EX)
-      yaml = YAML.safe_load(file.read, *args) || {} rescue {}
+      yaml = YAML.safe_load(file.read, *args) || {}
       yield yaml
       file.rewind
       file.write YAML.dump(yaml)
@@ -29,10 +32,12 @@ module YamlFile
   # Opens the file read-only, with a shared lock, and parses the YAML
   # This is yielded to the block (if provided), whilst holding the lock
   # Otherwise the YAML is returned to the caller, and the lock is released
+  # The args are passed to YAML.safe_load, and default to [Symbol]
   def self.read(yaml_file, *args)
+    args << [Symbol] if args.empty?
     File.open(yaml_file, File::RDONLY) do |file|
       file.flock(File::LOCK_SH)
-      yaml = YAML.safe_load(file.read,*args) || {} rescue {}
+      yaml = YAML.safe_load(file.read, *args)
       if block_given?
         yield yaml
       else
