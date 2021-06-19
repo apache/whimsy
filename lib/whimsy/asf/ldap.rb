ed30b3190745b4845fc8a240ca753e6b6fbfeb73
Sebb <sebb@apache.org>
2021-06-19 21:20:32 +0100

    Oops, left behind (not needed)

--- lib/whimsy/asf/ldap.rb
+++ lib/whimsy/asf/ldap.rb
@@ -902,7 +902,6 @@ module ASF
     # update an LDAP attribute for this person.  This needs to be run
     # either inside or after ASF::LDAP.bind.
     def modify(attr, value)
-      value = Array(value) 
       ASF::LDAP.modify(self.dn, [ASF::Base.mod_replace(attr.to_s, value)])
       # attributes are expected to be arrays
       attrs[attr.to_s] = value.is_a?(String) ? [value] : value
