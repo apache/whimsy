6deec727624e7ebaf4242cd623d5ef48b313bdf7
Sebb <sebb@apache.org>
2022-03-28 16:33:56 +0100

    Add test of just chromedriver

--- www/board/agenda/spec/navigate_spec.rb
+++ www/board/agenda/spec/navigate_spec.rb
@@ -4,6 +4,16 @@
 
 require_relative 'spec_helper'
 
+if ENV['TEST_AO_PAGE'] # code to test chromedriver only
+  feature 'chromedriver', js: true do
+    it 'should load ASF page' do
+      visit('http://apache.org')
+      expect(page).to have_content('The Apache Way')
+      expect(page).to have_no_content('The quick brown fox')
+    end
+  end
+end
+
 feature 'navigation', js: true do
   it "should navigate to the Cocoon report and back" do
     skip "headless browser test not run on Travis" if ENV['TRAVIS']
