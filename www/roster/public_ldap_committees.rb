db05abbe27cb5b37a4769f8ca19d9e084196a1de
Sebb <sebb@apache.org>
2022-04-14 23:35:32 +0100

    cronjob has been removed

--- www/roster/public_ldap_committees.rb
+++
@@ -1,4 +0,0 @@
-# Previously read LDAP ou=pmc,ou=committees,ou=groups,dc=apache,dc=org
-# but this is deprecated.
-# WHIMSY-386: now does nothing; awaiting removal of cron job before complete removal
-#
