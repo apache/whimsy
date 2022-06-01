ea7fe4aa38fde69fae5d6918e8ae888b463e67cc
Sebb <sebb@apache.org>
2022-06-01 12:55:47 +0100

    Docco

--- www/project/icla2/main.rb
+++ www/project/icla2/main.rb
@@ -1,13 +1,17 @@
+#!/usr/bin/env ruby
+
 require 'sinatra'
 
 get '/' do
   erb :index
 end
 
+# The submit button causes a POST to /answer, see below
 get '/form' do
   erb :form
 end
 
+# The data from the form will be passed parameters as e.g. params[:name]
 post '/answer' do
   erb :answer
 end
