d5f2785e65b3efb826c63d4fa1ecd7ab28d04dcd
John Bampton <jbampton@gmail.com>
2022-12-07 02:55:27 +1000

    Fix spelling

--- www/roster/main.rb
+++ www/roster/main.rb
@@ -237,7 +237,7 @@ get '/committer/:name' do |name|
 end
 
 post '/committer/:userid/:file' do |name, file|
-  # Workround for handling arrays
+  # Workaround for handling arrays
   # if the key :array_prefix is defined, the value is assumed to be the prefix for
   # a list of values with the names: prefix1, prefix2 etc
   # All non-empty values are collected and stored in an array which is added to the
