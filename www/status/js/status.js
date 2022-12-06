68ec8e909d650bb9c215e0bc03733d94c5c0cdda
Sebb <sebb@apache.org>
2022-12-06 22:37:15 +0000

    Replace deprecated click() calls

--- www/status/js/status.js
+++ www/status/js/status.js
@@ -93,11 +93,11 @@ $(function() {
 
       // expand all parents
       element.parents('.list-group').each(function() {
-        $('a[href="#' + this.getAttribute('id') + '"]').click();
+        $('a[href="#' + this.getAttribute('id') + '"]').trigger('click');
       });
 
       // expand this item
-      element.click()
+      element.trigger('click');
 
       // scroll to this item
       $('html, body').animate({scrollTop: element.offset().top}, 1000);
