4ba325156c9f37d3da11da07b7f80a77adcfdf29
Sebb <sebb@apache.org>
2023-05-15 23:47:31 +0100

    Try to debug failure

--- www/board/agenda/spec/secretary_spec.rb
+++ www/board/agenda/spec/secretary_spec.rb
@@ -11,6 +11,7 @@ feature 'report' do
 
   it "should allow timestamps to be edited" do
     visit '/2015-02-18/Call-to-order'
+    $stderr.puts page.html
     expect(page).to have_selector 'button', text: 'edit minutes'
   end
 
