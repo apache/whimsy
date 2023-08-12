ca6ff093d6d248dcec4c8a0ab57218809efce45c
Sebb <sebb@apache.org>
2023-08-12 13:58:28 +0100

    Add verbose (-v) option

--- lib/whimsy/utf8-utils.rb
+++ lib/whimsy/utf8-utils.rb
@@ -9,7 +9,7 @@ module UTF8Utils
   #
   # Initially assumes the file is in utf8-softbank encoding
   # If that does not work, then it tries ISO-8859-1
-  def self.repair(src, dst)
+  def self.repair(src, dst, verbose=false)
     opts = {undef: :replace, invalid: :replace}
     ec1 = Encoding::Converter.new('utf8-softbank', "UTF-8", **opts)
     ec2 = Encoding::Converter.new('iso-8859-1', "UTF-8", **opts)
@@ -20,6 +20,11 @@ module UTF8Utils
         unless o == l
           if o.include? UTF8_REPLACE # something did not convert
             o = ec2.convert(l) # try another encoding
+          end
+          if verbose
+            puts l
+            puts o
+            puts ''
           end
         end
         w.write o
