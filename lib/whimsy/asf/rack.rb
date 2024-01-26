32f36dbdad01e9dd95b4e852c3fda04f1f85c112
Sebb <sebb@apache.org>
2024-01-26 23:18:48 +0000

    Improved info

--- lib/whimsy/asf/rack.rb
+++ lib/whimsy/asf/rack.rb
@@ -18,7 +18,7 @@ module ASF
 
       if auth.to_s.empty?
         env.user = env['REMOTE_USER'] || ENV['USER'] || Etc.getpwuid.name
-        Wunderbar.warn "No auth found for #{env.user} at #{caller[0..2]}"
+        Wunderbar.warn "No auth found for #{env.user} at #{caller.select{|s| s =~ %r{/srv/whimsy/}}}}"
       else
         require 'base64'
         env.user, env.password = Base64.
