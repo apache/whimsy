abf9b38dcba615831780639a274c910f1d5c3ba2
Sebb <sebb@apache.org>
2024-08-06 11:10:13 +0100

    Tools to list available mailboxes

--- www/secretary/workbench/models/mailbox.rb
+++ www/secretary/workbench/models/mailbox.rb
@@ -34,6 +34,24 @@ class Mailbox
     system 'rsync', *options, source, "#{ARCHIVE}/"
   end
 
+  # Get list of mailboxes
+  def self.allmailboxes
+    current = Date.today.strftime('%Y%m.yml') # exclude future-dated entries
+    Dir.entries(ARCHIVE).select{|name| name =~ %r{^\d{6}\.yml$} && name <= current}.map{|n| File.basename n,'.yml'}.sort
+  end
+
+  # get prev and next entries (or nil)
+  def self.prev_next(current)
+    active = self.allmailboxes
+    index = active.find_index current
+    prv = nxt = nil
+    if index
+      prv = active[index - 1] if index > 0
+      nxt = active[index + 1] if index < active.length
+    end
+    return prv, nxt
+  end
+
   #
   # Initialize a mailbox
   #
