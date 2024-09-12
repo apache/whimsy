59b8a2c9add8dc60c1a67a48f6c9e7dd034d62ca
Sebb <sebb@apache.org>
2024-09-12 14:30:06 +0100

    Add withdrawal_request; simplify

--- www/roster/views/person/forms.js.rb
+++ www/roster/views/person/forms.js.rb
@@ -4,6 +4,15 @@
 
 class PersonForms < Vue
   def render
+    form_names = {
+      icla: 'ICLA',
+      member: 'Member App',
+      emeritus: 'Emeritus',
+      emeritus_request: 'Emeritus Request',
+      emeritus_rescinded: 'Emeritus Rescinded',
+      emeritus_reinstated: 'Emeritus Reinstated',
+      withdrawal_request: 'Withdrawal Request',
+    }
     committer = @@person.state.committer
 
     _div.row do
@@ -13,67 +22,20 @@ class PersonForms < Vue
         _ul do
           for form in committer.forms
             link = committer.forms[form]
-
-            if form == 'icla'
-              _li do
-                if link == '' # has ICLA but no karma to view it
-                  _ 'ICLA'
-                else
-                  _a 'ICLA', href: link
-                end
+            link_name = form_names[form] || link_name
+            _li do
+              if link == '' # has form but no karma to view it
+                _ link_name
+              else
+                _a link_name, href: link
               end
-            elsif form == 'member'
-              _li do
-                if link == '' # has form but no karma to view it
-                  _ 'Member App'
-                else
-                  _a 'Member App',
-                    href: link
-                end
-              end
-            elsif form == 'emeritus'
-              _li do
-                if link == '' # has form but no karma to view it
-                  _ 'Emeritus'
-                else
-                  _a 'Emeritus',
-                    href: link
-                end
-              end
-            elsif form == 'emeritus_request'
-              _li do
-                if link == '' # has form but no karma to view it
-                  _ 'Emeritus Request'
-                else
-                  _a 'Emeritus Request',
-                    href: link
-                end
+              if form == 'emeritus_request'
                 emeritus_request_age = committer['emeritus_request_age']
                 if emeritus_request_age
                   _ ' Days since submission: '
                   _ emeritus_request_age
                 end
               end
-            elsif form == 'emeritus_rescinded'
-              _li do
-                if link == '' # has form but no karma to view it
-                  _ 'Emeritus Rescinded'
-                else
-                  _a 'Emeritus Rescinded',
-                    href: link
-                end
-              end
-            elsif form == 'emeritus_reinstated'
-              _li do
-                if link == '' # has form but no karma to view it
-                  _ 'Emeritus Reinstated'
-                else
-                  _a 'Emeritus Reinstated',
-                    href: link
-                end
-              end
-            else
-              _li "#{form}: #{link}"
             end
           end
         end
