485d074689c8935f0ed048d6a747cdb135f8ac0b
Sebb <sebb@apache.org>
2025-06-25 23:42:17 +0100

    Also need an action script to process the template

---
+++ www/secretary/workbench/views/actions/clarify_intent.json.rb
@@ -0,0 +1,41 @@
+# extract message
+message = Mailbox.find(@message)
+
+# obtain per-user information
+_personalize_email(env.user)
+
+# extract/verify project
+_extract_project
+
+########################################################################
+#                           email submitter                            #
+########################################################################
+
+# send rejection email
+task "email #{message.from}" do
+  # build mail from template
+  @email = message.from
+  mail = message.reply(
+    from: @from,
+    cc: [
+      'secretary@apache.org',
+      (@pmc.private_mail_list if @pmc), # copy pmc
+      (@podling.private_mail_list if @podling) # copy podling
+    ],
+    body: template('clarify-intent.erb')
+  )
+
+  # echo email
+  form do
+    _message mail.to_s
+  end
+
+  # deliver mail
+  complete do
+    _disposition :keep # set this first in case mail sending fails
+
+    mail.deliver!
+
+    _status 'request to clarify intent has been sent.'
+  end
+end
