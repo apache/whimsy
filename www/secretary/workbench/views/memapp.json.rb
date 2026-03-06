f0750fd969b57144d337d5e479a22ca82c9b7c33
Sebb <sebb@apache.org>
2026-03-06 13:12:03 +0000

    Ignore template line

--- www/secretary/workbench/views/memapp.json.rb
+++ www/secretary/workbench/views/memapp.json.rb
@@ -3,6 +3,6 @@ require 'whimsy/asf/meeting-util'
 # parse and return the contents of the latest memapp-received file
 
 # Application expiry is now handled by the GUI
-table = ASF::MeetingUtil.parse_memapp_to_h.sort_by{|k| k[:name]}
+table = ASF::MeetingUtil.parse_memapp_to_h.reject{|k| k[:id]=='<id>'}.sort_by{|k| k[:name]}
 
 {received: table}
